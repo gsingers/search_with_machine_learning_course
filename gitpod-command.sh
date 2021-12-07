859db402672f8c4387465b1f78c6367519ee57ec
gsingers <gsingers@apache.org>
2021-12-07 11:47:25 -0500

    more tweaks

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -1,3 +1,7 @@
 mkdir -p /workspace/opensearch
+mkdir -p /workspace/logstash
+sudo chown -R gitpod:gitpod /workspace/opensearch
+sudo chown -R gitpod:gitpod /workspace/opensearch-data1
+sudo  chown -R gitpod:gitpod /workspace/logstash
 cd docker
-docker-compose up
+docker-compose up -d
