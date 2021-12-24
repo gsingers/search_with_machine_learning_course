079229f464daba52f7d2c5a4c35f2d78acf2ec65
gsingers <gsingers@apache.org>
2021-12-24 17:19:07 -0500

    week 1 examples

--- opensearch/opensearch_week1_toy.py
+++ opensearch/opensearch_week1_toy.py
@@ -2,24 +2,109 @@ from opensearchpy import OpenSearch
 
 host = 'localhost'
 port = 9200
-auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
+auth = ('admin', 'admin')  # For testing only. Don't store credentials in code.
 
-# Create the client with SSL/TLS enabled, but hostname verification disabled.
+# Create the client with SSL/TLS enabled, but hostname and certificate verification disabled.
 client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True, # enables gzip compression for request bodies
-    http_auth = auth,
+    hosts=[{'host': host, 'port': port}],
+    http_compress=True,  # enables gzip compression for request bodies
+    http_auth=auth,
     # client_cert = client_cert_path,
     # client_key = client_key_path,
-    use_ssl = True,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False,
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False,
 )
 
 # Do a few checks before we start indexing:
-client.cat.health()
-client.cat.indices()
+print(client.cat.health())
+print(client.cat.indices())
 
 # If you still have your documents from the Dev Tools test, we should be able to check them here:
-client.cat.count("searchml_test")
+print(client.cat.count("searchml_test", params={"v": "true"}))
+
+# Create an index with non-default settings.
+index_name = 'searchml_revisited'
+index_body = {
+    'settings': {
+        'index': {
+            'query': {
+                'default_field': "body"
+            }
+        }
+    }
+}
+
+response = client.indices.create(index_name, body=index_body)
+print('\nCreating index:')
+print(response)
+
+# Add our sample document to the index.
+docs = [
+    {
+        "id": "doc_a",
+        "title": "Fox and Hounds",
+        "body": "The quick red fox jumped over the lazy brown dogs.",
+        "price": "5.99",
+        "in_stock": True,
+        "category": "childrens"},
+    {
+        "id": "doc_b",
+        "title": "Fox wins championship",
+        "body": "Wearing all red, the Fox jumped out to a lead in the race over the Dog.",
+        "price": "15.13",
+        "in_stock": True,
+        "category": "sports"},
+    {
+        "id": "doc_c",
+        "title": "Lead Paint Removal",
+        "body": "All lead must be removed from the brown and red paint.",
+        "price": "150.21",
+        "in_stock": False,
+        "category": "instructional"},
+    {
+        "id": "doc_d",
+        "title": "The Three Little Pigs Revisted",
+        "price": "3.51",
+        "in_stock": True,
+        "body": "The big, bad wolf huffed and puffed and blew the house down. The end.",
+        "category": "childrens"}
+]
+
+for doc in docs:
+    doc_id = doc["id"]
+    print("Indexing {}".format(doc_id))
+    response = client.index(
+        index=index_name,
+        body=doc,
+        id=doc_id,
+        refresh=True
+    )
+    print('\n\tResponse:')
+    print(response)
+
+# Verify they are in:
+print(client.cat.count(index_name, params={"v": "true"}))
+
+
+# if you want to delete the documents, but keep the index, run the following:
+for doc in docs:
+    doc_id = doc["id"]
+    print("Indexing {}".format(doc_id))
+    response = client.delete(
+        index=index_name,
+        id=doc_id,
+    )
+    print('\n\tResponse:')
+    print(response)
+
+
+# If at any time you want to start over, run this command to delete the index and then you can start from the toop
+# Delete the index.
+response = client.indices.delete(
+    index = index_name
+)
+
+print('\nDeleting index:')
+print(response)
