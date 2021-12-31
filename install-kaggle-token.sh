4ef660e45643d73af237fb5704954efaa17bd424
gsingers <gsingers@apache.org>
2021-12-31 15:48:49 -0500

    add in placeholders for field mappings, update indexing script

--- install-kaggle-token.sh
+++ install-kaggle-token.sh
@@ -1,5 +1,5 @@
-mkdir -p home/gitpod/.kaggle
-read -p "Please enter your Kaggle API token here (e.g. {\"username\":\"corise\",\"key\":\"6ebf9bb14dde51cf09aca7604de762ak\"})" api_token
+
+read -p "Please enter your Kaggle API token here (e.g. {\"username\":\"corise\",\"key\":\"3536356fgdf\"})" api_token
 echo "Your Kaggle API token is:" $api_token
 echo "Installing Kaggle API token to /home/gitpod/.kaggle/kaggle.json ..."
 echo $api_token > /home/gitpod/.kaggle/kaggle.json
