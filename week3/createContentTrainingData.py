59f9845cd607de3b677e8470252b265d9b7df0a4
dtunkelang <dtunkelang@gmail.com>
2022-01-22 15:26:08 -0800

    Update createContentTrainingData.py
    simplified so student have to do more of the work

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -1,12 +1,8 @@
 import os
 import xml.etree.ElementTree as ET
 
-directory = r'/workspace/datasets/bbuy/product_data/products/'
-min_count = 1000
-max_count = 1000
-
-counts = {}
-data = []
+# Directory for product data
+directory = r'/workspace/datasets/product_data/products/'
 
 for filename in os.listdir(directory):
     if filename.endswith(".xml"):
@@ -14,19 +10,15 @@ for filename in os.listdir(directory):
         tree = ET.parse(f)
         root = tree.getroot()
         for child in root:
-            if (child.find('name') is not None and \
-                child.find('name').text is not None and \
-                child.find('categoryPath') is not None and \
-                len(child.find('categoryPath')) > 0 and \
+            
+            # Check to make sure category name is valid
+            if (child.find('name') is not None and child.find('name').text is not None and \
+                child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and \
                 child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
+                
+                  # Choose last element in categoryPath as the leaf categoryId
                   cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                  if cat not in counts:
-                      counts[cat] = 0
-                  if counts[cat] < max_count:
-                      name = child.find('name').text.replace('\n', ' ')
-                      data.append((cat, name))
-                      counts[cat] = counts[cat] + 1
-
-for entry in data:
-    if counts[entry[0]] >= min_count:
-        print('__label__' + entry[0] + " " + entry[1])
+                
+                  # Replace newline chars with spaces so fastText doesn't complain
+                  name = child.find('name').text.replace('\n', ' ')
+                  print('__label__' + cat + " " + name)
