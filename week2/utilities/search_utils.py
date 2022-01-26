6f555dd046653d8d95d963806af33ddccb07a35f
gsingers <gsingers@apache.org>
2022-01-26 18:29:19 -0500

    add in hand tuned

--- week2/utilities/search_utils.py
+++ week2/utilities/search_utils.py
@@ -19,7 +19,8 @@ def evaluate_test_set(test_data, all_clicks_df, opensearch, xgb_model_name, ltr_
     source = ["sku", "name"]
     ltr_results = {}
     baseline_results = {}
-    results = {"baseline": baseline_results, "ltr": ltr_results}
+    hand_tuned_results = {}
+    results = {"baseline": baseline_results, "ltr": ltr_results, "hand_tuned": hand_tuned_results}
     print("Running test queries")
     for key in query_gb.groups.keys():
         all_clicks_for_query = query_all_gb.get_group(key) # this is the set of docs in our sample that have clicks
@@ -30,6 +31,10 @@ def evaluate_test_set(test_data, all_clicks_df, opensearch, xgb_model_name, ltr_
         query_object = qu.create_simple_baseline(key, filters=None, size=size, highlight=False, include_aggs=False, source=source)
 
         baseline_results[key] = __judge_hits(all_skus_for_query, index, key, no_results, opensearch, precision, query_object)
+        # Run hand-tuned
+        query_object = qu.create_query(key, filters=None, size=size, highlight=False, include_aggs=False, source=source)
+
+        hand_tuned_results[key] = __judge_hits(all_skus_for_query, index, key, no_results, opensearch, precision, query_object)
         #print(len(baseline_results[key]))
         # Score with LTR
         #print("\tRunning LTR:")
@@ -44,30 +49,38 @@ def evaluate_test_set(test_data, all_clicks_df, opensearch, xgb_model_name, ltr_
     print("Results analysis:")
     base_set = set(baseline_results.keys())
     ltr_set = set(ltr_results.keys())
+    hand_set = set(hand_tuned_results.keys())
     with (open(output_diffs, 'w')) as od:
-        query_diff = base_set.symmetric_difference(ltr_set)
-        if len(query_diff) > 0:
-            od.write("\tQuery diffs: %s\n" % query_diff)
-        for key in baseline_results.keys():
-            od.write("\tQuery: %s\n" % key)
-            base_matches = baseline_results.get(key)
-            ltr_matches = ltr_results.get(key)
-            if base_matches is None or len(base_matches) == 0:
-                od.write("\t\tNo base matches\n")
-            if ltr_matches is None or len(ltr_matches) == 0:
-                od.write("\t\tNo LTR matches\n")
-            # print any disagreements
-            base_sku_set = set(base_matches.items())
-            ltr_sku_set = set(ltr_matches.items())
-            sku_rank_diff = base_sku_set.difference(ltr_sku_set)
-            if len(sku_rank_diff) > 0:
-                od.write("\t\tResults diff (sku, rank): %s\n" % sku_rank_diff)
+        od.write("Writing out diffs between baseline and LTR:\n")
+        write_diffs(base_set, baseline_results, ltr_results, ltr_set, od)
+        od.write("Writing out diffs between hand tuned and LTR:\n")
+        write_diffs(hand_set, hand_tuned_results, ltr_results, ltr_set, od)
 
     # Caveat emmptor: precision is hard to define here, we're inferring a prior click as meaning the result is relevant.
     # This is self-serving, but really this whole thing is just trying to learn clicks
     print("Baseline p@%s is %.3f" % (precision, __calculate_precision(baseline_results, precision)))
     print("LTR p@%s is %.3f" % (precision,__calculate_precision(ltr_results, precision)))
     return (results, no_results)
+
+
+def write_diffs(to_compare_set, to_compare_results, ltr_results, ltr_set, od):
+    diff = to_compare_set.symmetric_difference(ltr_set)
+    if len(diff) > 0:
+        od.write("\tQuery diffs: %s\n" % diff)
+    for key in to_compare_results.keys():
+        od.write("\tQuery: %s\n" % key)
+        base_matches = to_compare_results.get(key)
+        ltr_matches = ltr_results.get(key)
+        if base_matches is None or len(base_matches) == 0:
+            od.write("\t\tNo base matches\n")
+        if ltr_matches is None or len(ltr_matches) == 0:
+            od.write("\t\tNo LTR matches\n")
+        # print any disagreements
+        base_sku_set = set(base_matches.items())
+        ltr_sku_set = set(ltr_matches.items())
+        sku_rank_diff = base_sku_set.difference(ltr_sku_set)
+        if len(sku_rank_diff) > 0:
+            od.write("\t\tResults diff (sku, rank): %s\n" % sku_rank_diff)
 
 
 # Result is a dict with the keys being the skus that have clicks in the original click model and the value is the position in the
