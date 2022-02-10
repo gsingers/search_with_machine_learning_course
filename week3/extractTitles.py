9553adb6926149c4d3af2116e5845b547a60dae2
gsingers <gsingers@apache.org>
2022-02-10 14:21:27 -0500

    some comments, update search to take index, update with argparse

--- week3/extractTitles.py
+++ week3/extractTitles.py
@@ -1,19 +1,40 @@
 import os
 import xml.etree.ElementTree as ET
+import argparse
+from pathlib import Path
+
+
+directory = r'/workspace/datasets/product_data/products/'
+parser = argparse.ArgumentParser(description='Process some integers.')
+general = parser.add_argument_group("general")
+general.add_argument("--input", default=directory,  help="The directory containing the products")
+general.add_argument("--output", default="/workspace/datasets/fasttext/titles.txt", help="the file to output to")
+
+args = parser.parse_args()
+output_file = args.output
+path = Path(output_file)
+output_dir = path.parent
+if os.path.isdir(output_dir) == False:
+        os.mkdir(output_dir)
+
+if args.input:
+    directory = args.input
+
 
 def transform_training_data(name):
     # IMPLEMENT
     return name.replace('\n', ' ')
 
 # Directory for product data
-directory = r'/workspace/datasets/product_data/products/'
 
-for filename in os.listdir(directory):
-    if filename.endswith(".xml"):
-        f = os.path.join(directory, filename)
-        tree = ET.parse(f)
-        root = tree.getroot()
-        for child in root:
-            if (child.find('name') is not None and child.find('name').text is not None):
-                name = transform_training_data(child.find('name').text)
-                print(name)
+print("Writing results to %s" % output_file)
+with open(output_file, 'w') as output:
+    for filename in os.listdir(directory):
+        if filename.endswith(".xml"):
+            f = os.path.join(directory, filename)
+            tree = ET.parse(f)
+            root = tree.getroot()
+            for child in root:
+                if (child.find('name') is not None and child.find('name').text is not None):
+                    name = transform_training_data(child.find('name').text)
+                    output.write(name + "\n")
--- week4/leavesToPaths.py
+++ week4/leavesToPaths.py
@@ -1,14 +1,22 @@
 import sys
 import os
 import xml.etree.ElementTree as ET
+import argparse
+from pathlib import Path
 
 # Location for category data                                                                                                                                                               
 categoriesFilename = '/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
+parser = argparse.ArgumentParser(description='Process some integers.')
+general = parser.add_argument_group("general")
+general.add_argument("--input", default=categoriesFilename,  help="The full path to the filename containing the categories")
+general.add_argument("--max_depth", default=0, type=int, help="the file to output to")
 
-# Optional arg to specify max depth of category tree                                                                                                                                       
-maxDepth = 0
-if (len(sys.argv) >- 2):
-    maxDepth = int(sys.argv[1])
+args = parser.parse_args()
+
+categoriesFilename = args.input
+
+# Optional arg to specify max depth of category tree
+maxDepth = args.max_depth
 
 tree = ET.parse(categoriesFilename)
 root = tree.getroot()
