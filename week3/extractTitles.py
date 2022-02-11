b15e3b396bb9fab778620818a5cac770782f1ff4
dtunkelang <dtunkelang@gmail.com>
2022-02-11 15:25:30 -0800

    Update extractTitles.py
    copied from search_course

--- week3/extractTitles.py
+++ week3/extractTitles.py
@@ -10,6 +10,9 @@ general = parser.add_argument_group("general")
 general.add_argument("--input", default=directory,  help="The directory containing the products")
 general.add_argument("--output", default="/workspace/datasets/fasttext/titles.txt", help="the file to output to")
 
+# Consuming all of the product data takes a while. But we still want to be able to obtain a representative sample.
+general.add_argument("--sample_rate", default=0.1, type=float, help="The rate at which to sample input (default is 0.1)")
+
 args = parser.parse_args()
 output_file = args.output
 path = Path(output_file)
@@ -20,6 +23,7 @@ if os.path.isdir(output_dir) == False:
 if args.input:
     directory = args.input
 
+sample_rate = args.sample_rate
 
 def transform_training_data(name):
     # IMPLEMENT
@@ -35,6 +39,8 @@ with open(output_file, 'w') as output:
             tree = ET.parse(f)
             root = tree.getroot()
             for child in root:
+                if random.random() > sample_rate:
+                    continue
                 if (child.find('name') is not None and child.find('name').text is not None):
                     name = transform_training_data(child.find('name').text)
                     output.write(name + "\n")
