9e4ab9e11b2f5f0ce59dddf9c6e8c813b05181c0
André Tzermias <andreportaro@gmail.com>
2022-02-15 02:59:53 +0000

    Create opensearch connection

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -5,6 +5,14 @@ from opensearchpy import OpenSearch
 def get_opensearch():
     if 'opensearch' not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
-        g.opensearch = None
+        g.opensearch =  OpenSearch(
+            'https://localhost:9200',
+            http_auth = ('admin', 'admin'),
+            ssl_show_warn = False,
+            # turn on SSL
+            use_ssl=False,
+            # make sure we verify SSL certificates
+            verify_certs=False
+        )
 
     return g.opensearch
