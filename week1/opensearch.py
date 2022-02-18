10f77133a5cc1f674d60a2ed5e4506a86095e7bd
Zack Kneupper <zachary.kneupper@gmail.com>
2022-02-18 18:56:10 +0000

    Create OpenSearch client.

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -15,17 +15,31 @@ from flask import g, current_app
 from opensearchpy import OpenSearch
 
 
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 9200
 
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
-    if 'opensearch' not in g:
+    if "opensearch" not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
         # g.opensearch = None
 
-        # auth = (os.environ['OPENSEARCH_USERNAME'], os.environ['OPENSEARCH_PW'])
-        g.opensearch = None
+        auth = (
+            os.environ.get("OPENSEARCH_USERNAME", "<add_fallback>"),
+            os.environ.get("OPENSEARCH_PW", "<add_fallback>")
+        )
 
+        # Create the client with SSL/TLS disabled, and hostname verification disabled.
+        client = OpenSearch(
+            hosts = [{"host": HOST, "port": PORT}],
+            http_compress = True, # enables gzip compression for request bodies
+            http_auth = auth,
+            use_ssl = False,
+            verify_certs = False,
+            ssl_assert_hostname = False,
+            ssl_show_warn = False
+        )
 
+        g.opensearch = client
+q
     return g.opensearch
