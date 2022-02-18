25a6ada6caa099d65a6843c68b7b8970f466385b
Zack Kneupper <zachary.kneupper@gmail.com>
2022-02-18 18:26:05 +0000

    Set host and port for opensearch client

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -10,13 +10,22 @@ References:
 > https://github.com/opensearch-project/opensearch-py
 """
 
+import os
 from flask import g, current_app
 from opensearchpy import OpenSearch
+
+
+HOST = 'localhost'
+PORT = 9200
 
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if 'opensearch' not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
+        # g.opensearch = None
+
+        # auth = (os.environ['OPENSEARCH_USERNAME'], os.environ['OPENSEARCH_PW'])
         g.opensearch = None
+
 
     return g.opensearch
