bc5b252a6f232c789fb8c4925ff0619d4e1cf324
Zack Kneupper <zachary.kneupper@gmail.com>
2022-02-18 18:02:46 +0000

    Add docstring to opensearch.py

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""The `opensearch.py` module defines the `get_opensearch()` function
+that creates an OpenSearch client instance, makes that client available
+in flask's global namespace `g` by setting `g.opensearch`, and returns
+the OpenSearch client.
+
+References:
+> See opensearch-py docs:
+> https://github.com/opensearch-project/opensearch-py
+"""
+
 from flask import g, current_app
 from opensearchpy import OpenSearch
 
