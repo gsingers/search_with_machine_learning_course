1176a4520dad8cc8bac8fc687cace2729eeb4e11
Zack Kneupper <zachary.kneupper@gmail.com>
2022-02-18 22:30:01 +0000

    Set use_ssl=True when creating OpenSearch python client.

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -17,14 +17,13 @@ from flask import g, current_app
 from opensearchpy import OpenSearch
 
 
-HOST = "localhost"
-PORT = 9200
-
-
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if "opensearch" not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
+
+        HOST = "localhost"
+        PORT = 9200
 
         load_dotenv()
 
@@ -38,7 +37,7 @@ def get_opensearch():
             hosts=[{"host": HOST, "port": PORT}],
             http_compress=True,  # enables gzip compression for request bodies
             http_auth=auth,
-            use_ssl=False,
+            use_ssl=True,
             verify_certs=False,
             ssl_assert_hostname=False,
             ssl_show_warn=False,
