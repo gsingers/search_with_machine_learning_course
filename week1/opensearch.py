473c20400e067bab766bae8a61721edad0b7491c
Manikandan Sivanesan` <msivanes31@gmail.com>
2022-02-18 04:13:35 +0000

    add opensearch client instance

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -1,10 +1,14 @@
 from flask import g, current_app
 from opensearchpy import OpenSearch
 
+import json
+
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if 'opensearch' not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
-        g.opensearch = None
+        host, port = 'localhost', 9200
+        auth=('admin', 'admin')
+        g.opensearch = OpenSearch(hosts=[{'host': host, 'port': port}], http_compress=True, http_auth=auth, use_ssl=True, verify_certs=False, ssl_assert_hostname=False, ssl_show_warn=False)
 
     return g.opensearch
