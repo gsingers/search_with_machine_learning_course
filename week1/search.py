1ef77721aee67b0fd3a91ff1b6d95d7ceae01d10
Ovidiu Mihalcea <ovidiu@faction.ro>
2022-02-20 15:46:39 +0000

    Completed week1

--- week1/search.py
+++ week1/search.py
@@ -1,6 +1,7 @@
 #
 # The main search hooks for the Search Flask application.
 #
+from urllib import response
 from flask import (
     Blueprint, redirect, render_template, request, url_for
 )
@@ -22,20 +23,27 @@ def process_filters(filters_input):
     for filter in filters_input:
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName", filter)
-        #
-        # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
-        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
-                                                                                 display_name)
-        #TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
-        # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
+        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter, display_name)
+
         if type == "range":
-            pass
+            key = request.args.get(filter + ".key", filter)
+            FROM = request.args.get(filter + ".from", filter)
+            TO = request.args.get(filter + ".to", filter)
+            applied_filters += f'''&{filter}.key={key}&{filter}.from={FROM}&{filter}.to={TO}'''
+            if FROM and TO: range_filter = {"range": {"regularPrice": { "lt": TO, "gte": FROM }}}
+            if FROM and not TO: range_filter = {"range": {"regularPrice": {"gte": FROM}}}
+            if TO and not FROM: range_filter = {"range": {"regularPrice": {"lt": TO}}}
+            filters.append(range_filter)
+            display_filters.append(f"Price: {FROM} TO {TO}")
         elif type == "terms":
-            pass #TODO: IMPLEMENT
-    print("Filters: {}".format(filters))
+            key = request.args.get(filter + ".key", filter)
+            applied_filters += f'''&{filter}.key={key}'''
+            term_filter = {"term": {"department.keyword": key}}
+            filters.append(term_filter)
+            display_filters.append(f"Department: {key}")
+    # print("Filters: {}".format(filters))
 
     return filters, display_filters, applied_filters
-
 
 # Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
 @bp.route('/query', methods=['GET', 'POST'])
@@ -73,11 +81,7 @@ def query():
     else:
         query_obj = create_query("*", [], sort, sortDir)
 
-    print("query obj: {}".format(query_obj))
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
-    # Postprocess results here if you so desire
-
-    #print(response)
+    response = opensearch.search(index='bbuy_products', body=query_obj)
     if error is None:
         return render_template("search_results.jinja2", query=user_query, search_response=response,
                                display_filters=display_filters, applied_filters=applied_filters,
@@ -87,14 +91,158 @@ def query():
 
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
-    print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
-    query_obj = {
-        'size': 10,
-        "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
-        },
-        "aggs": {
-            #TODO: FILL ME IN
+    # print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
+
+    multi_match_with_filter = {
+        "bool": {
+            "must": [{
+                    "multi_match": {
+                        "query": user_query,
+                        "type": "cross_fields",
+                        "analyzer": "english",
+                        "minimum_should_match": "100%",
+                        "fields": [
+                            "manufacturer^100",
+                            "name^50",
+                            "categoryPath^25",
+                            "department"
+                        ],
+                    },
+                }],
+            "should": [
+                {
+                    "match_phrase": {
+                        "shortDescription": {
+                            "query": user_query, "analyzer": "english"
+                        }
+                    }
+                }
+            ],
+            "filter": filters,
         }
+    }
+
+    match_all_with_filter = {
+        "bool": {
+            "must": [{"match_all": {}}],
+            "filter": filters
+        }
+    }
+
+    match_obj = match_all_with_filter if user_query == '*' else multi_match_with_filter
+
+    query_obj = {
+        "query": {
+            "function_score": {
+                "query": match_obj,
+                "functions": [
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankShortTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankMediumTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankLongTerm",
+                            "modifier": "reciprocal",
+                            "missing": 100000000
+                        }
+                    }
+                ],
+                "boost_mode": "multiply",
+                "score_mode": "avg"
+            }
+        },
+        "from": 0,
+        "size": 100,
+        "aggregations": {
+            "brands": {
+                "terms": {
+                    "field": "manufacturer.keyword"
+                }
+            },
+            "department": {
+                "terms": {
+                    "field": "department.keyword"
+                }
+            },
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        {
+                            "key": "*-100",
+                            "to": 100
+                        },
+                        {
+                            "key": "100-200",
+                            "from": 100,
+                            "to": 200
+                        },
+                        {
+                            "key": "200-500",
+                            "from": 200,
+                            "to": 500
+                        },
+                        {
+                            "key": "500-1000",
+                            "from": 500,
+                            "to": 1000
+                        },
+                        {
+                            "key": "1000-2000",
+                            "from": 1000,
+                            "to": 2000
+                        },
+                        {
+                            "key": "2000-5000",
+                            "from": 2000,
+                            "to": 5000
+                        },
+                        {
+                            "key": "5000-*",
+                            "from": 5000
+                        }
+                    ]
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image.keyword"
+                }
+            }
+        },
+        "_source": [
+            "productId",
+            "name",
+            "categoryPath",
+            "image",
+            "active",
+            "regularPrice",
+            "salePrice",
+            "onSale",
+            "url",
+            "shortDescription",
+            "department",
+            "departmentId",
+            "manufacturer"
+        ],
+        "highlight": {
+            "fields": {
+                "*": {}
+            }
+        },
+        "sort": [{
+            sort: {"order": sortDir}
+        }]
     }
     return query_obj
