55099881d516d67fc1b2cbe97080a770a5da1c94
Andrew Cheung <atclate@hotmail.com>
2022-02-21 03:22:03 +0000

    Homework 1

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -1,10 +1,25 @@
 from flask import g, current_app
 from opensearchpy import OpenSearch
 
+host = 'localhost'
+port = 9200
+auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
+
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if 'opensearch' not in g:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
-        g.opensearch = None
+        print("initializing client")
+        g.opensearch = OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_compress = True, # enables gzip compression for request bodies
+            http_auth = auth,
+            # client_cert = client_cert_path,
+            # client_key = client_key_path,
+            use_ssl = True,
+            verify_certs = False,
+            ssl_assert_hostname = False,
+            ssl_show_warn = False,
+        )
 
     return g.opensearch
--- week1/search.py
+++ week1/search.py
@@ -7,6 +7,8 @@ from flask import (
 
 from week1.opensearch import get_opensearch
 
+from json import dumps
+
 bp = Blueprint('search', __name__, url_prefix='/search')
 
 
@@ -17,7 +19,8 @@ bp = Blueprint('search', __name__, url_prefix='/search')
 def process_filters(filters_input):
     # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
-    display_filters = []  # Also create the text we will use to display the filters that are applied
+    # Also create the text we will use to display the filters that are applied
+    display_filters = []
     applied_filters = ""
     for filter in filters_input:
         type = request.args.get(filter + ".type")
@@ -26,12 +29,29 @@ def process_filters(filters_input):
         # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
         applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
                                                                                  display_name)
-        #TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
+        # TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
         # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
         if type == "range":
+            # Example:
+            # "filter": [
+            #     { "range": { "publish_date": { "gte": "2015-01-01" }}}
+            # ]
+            filter_key = {}
+            if request.args.get(filter + ".from"):
+                filter_key["gte"] = request.args.get(filter + ".from")
+            if request.args.get(filter + ".to"):
+                filter_key["lte"] = request.args.get(filter + ".to")
+            if filter_key:
+                range_filter_name = {}
+                range_filter_name[filter] = filter_key
+                range_filter = {"range": range_filter_name}
+                filters.append(range_filter)
             pass
         elif type == "terms":
-            pass #TODO: IMPLEMENT
+            # "filter": [
+            #     { "term":  { "status": "published" }}
+            # ]
+            pass  # TODO: IMPLEMENT
     print("Filters: {}".format(filters))
 
     return filters, display_filters, applied_filters
@@ -40,16 +60,21 @@ def process_filters(filters_input):
 # Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
 @bp.route('/query', methods=['GET', 'POST'])
 def query():
-    opensearch = get_opensearch() # Load up our OpenSearch client from the opensearch.py file.
+    # Load up our OpenSearch client from the opensearch.py file.
+    opensearch = get_opensearch()
     # Put in your code to query opensearch.  Set error as appropriate.
     error = None
     user_query = None
     query_obj = None
     display_filters = None
     applied_filters = ""
-    filters = None
+    filters = []
     sort = "_score"
     sortDir = "desc"
+
+    print("request.args")
+    print(dumps(request.args, indent=4, sort_keys=True))
+
     if request.method == 'POST':  # a query has been submitted
         user_query = request.form['query']
         if not user_query:
@@ -74,10 +99,13 @@ def query():
         query_obj = create_query("*", [], sort, sortDir)
 
     print("query obj: {}".format(query_obj))
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
+    response = opensearch.search(
+        body=query_obj,
+        index="bbuy_products"
+    )
     # Postprocess results here if you so desire
 
-    #print(response)
+    # print(response)
     if error is None:
         return render_template("search_results.jinja2", query=user_query, search_response=response,
                                display_filters=display_filters, applied_filters=applied_filters,
@@ -88,13 +116,111 @@ def query():
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
     print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
+    bool_query = {
+        "should": [
+            {
+                "query_string": {
+                    "query": user_query,
+                    "type": "phrase",
+                    "fields": ["name^10", "name.hyphens^10", "shortDescription^5",
+                               "longDescription^5", "department^0.5", "sku", "manufacturer", "features", "categoryPath"]
+                }
+            }
+        ]
+    }
+    if filters:
+        bool_query['filter'] = filters
     query_obj = {
         'size': 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "function_score": {
+                "query": {
+                    "bool": bool_query
+                },
+                "boost_mode": "multiply",  # how _score and functions are combined
+                "score_mode": "sum",  # how functions are combined
+                "functions": [
+                    {
+                        "filter": {
+                            "exists": {
+                                "field": "salesRankShortTerm"
+                            }
+                        },
+                        "gauss": {
+                            "salesRankShortTerm": {
+                                "origin": "1.0",
+                                "scale": "100"
+                            }
+                        }
+                    },
+                    {
+                        "filter": {
+                            "exists": {
+                                "field": "salesRankMediumTerm"
+                            }
+                        },
+                        "gauss": {
+                            "salesRankMediumTerm": {
+                                "origin": "1.0",
+                                "scale": "1000"
+                            }
+                        }
+                    },
+                    {
+                        "filter": {
+                            "exists": {
+                                "field": "salesRankLongTerm"
+                            }
+                        },
+                        "gauss": {
+                            "salesRankLongTerm": {
+                                "origin": "1.0",
+                                "scale": "1000"
+                            }
+                        }
+                    },
+                    {
+                        "script_score": {
+                            "script": "0.0001"
+                        }
+                    }
+                ]
+            }
         },
         "aggs": {
-            #TODO: FILL ME IN
+            "class": {
+                "terms": {
+                    "field": "class.keyword"
+                }
+            },
+            "department": {
+                "terms": {
+                    "field": "department.keyword"
+                }
+            },
+            "type": {
+                "terms": {
+                    "field": "type.keyword"
+                }
+            },
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        {"key": "$", "to": 100.0},
+                        {"key": "$$", "from": 100.0, "to": 200.0},
+                        {"key": "$$$", "from": 200.0, "to": 300.0},
+                        {"key": "$$$$", "from": 300.0, "to": 400.0},
+                        {"key": "$$$$$", "from": 400.0, "to": 500.0},
+                        {"key": "$$$$$$", "from": 500.0}
+                    ]
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image.keyword"
+                }
+            }
         }
     }
     return query_obj
