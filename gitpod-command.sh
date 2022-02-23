8776c121235bc479eca473d549d4bdada479cd4f
gsingers <gsingers@apache.org>
2022-02-23 11:35:03 -0500

    try copy

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -7,14 +7,6 @@ mkdir -p /workspace/datasets
 sudo chown -R gitpod:gitpod /workspace/opensearch
 sudo  chown -R gitpod:gitpod /workspace/logstash
 ln -s /home/gitpod/logstash-7.13.2 /workspace/logstash/logstash-7.13.2
-pyenv activate search_with_ml_week1
-pip install -r /workspace/search_with_machine_learning_course/requirements_week1.txt
-pyenv activate search_with_ml_week2
-pip install -r /workspace/search_with_machine_learning_course/requirements_week2.txt
-pyenv activate search_with_ml_week3
-pip install -r /workspace/search_with_machine_learning_course/requirements_week3.txt
-pyenv activate search_with_ml_week4
-pip install -r /workspace/search_with_machine_learning_course/requirements_week4.txt
 
 
 cd docker
