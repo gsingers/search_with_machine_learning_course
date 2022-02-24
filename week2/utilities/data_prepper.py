2473c5b6cfc16343a12198009654186b474e1b30
Matthew Barga <mail@matthewbarga.com>
2022-02-24 20:46:32 +0000

    week 2

--- week2/utilities/data_prepper.py
+++ week2/utilities/data_prepper.py
@@ -233,9 +233,40 @@ class DataPrepper:
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
+        try:
+            response = self.opensearch.search(body=log_query, index=self.index_name)
+        except RequestError as re:
+            print("Error logging features", re, log_query)
+        else:
+            if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
+                hits = response['hits']['hits']
+                feature_results = {}
+                feature_results["doc_id"] = []
+                feature_results["query_id"] = []
+                feature_results["sku"] = [] 
+                for (idx, hit) in enumerate(hits):
+                    features = hit['fields']['_ltrlog'][0]['log_entry']
+                    feature_results["doc_id"].append(int(hit['_id']))
+                    feature_results["sku"].append(int(hit['_source']['sku'][0]))
+                    feature_results["query_id"].append(
+                        int(query_id))  
+                    for feat_idx, feature in enumerate(features):
+                        feat_name = feature.get('name')
+                        feat_val = feature.get('value', 0)
+                        feat_vals = feature_results.get(feat_name)
+                        if feat_vals is None:
+                            feat_vals = []
+                            feature_results[feat_name] = feat_vals
+                        feat_vals.append(feat_val)
+                frame = pd.DataFrame(feature_results)
+                return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
+            else:  
+                no_results[key] = query_doc_ids
+        return None
+
+
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
