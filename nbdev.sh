4df4c42fd4d0ec7d0d82facd159648478b273921
Manikandan Sivanesan` <msivanes31@gmail.com>
2022-02-25 14:14:54 +0000

    WIP Checkpoint

--- nbdev.sh
+++ nbdev.sh
@@ -1,3 +1,4 @@
-pip install jupyter jupyterlab nbdev rich fastcore
+pyenv activate search_with_ml_week2
+pip install jupyter jupyterlab nbdev rich fastcore -qq
 # The jupyter notebook UI can be access from https:///8888-gitpod-url
-# jupyter-lab --NotebookApp.allow_origin='*'
+jupyter-lab --NotebookApp.allow_origin='*'
--- week2/utilities/data_prepper.py
+++ week2/utilities/data_prepper.py
@@ -233,25 +233,39 @@ class DataPrepper:
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
-        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
-        # Your structure should look like the data frame below
         feature_results = {}
-        feature_results["doc_id"] = []  # capture the doc id so we can join later
-        feature_results["query_id"] = []  # ^^^
-        feature_results["sku"] = []
-        feature_results["salePrice"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
-            feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  # ^^^
-            feature_results["salePrice"].append(rng.random())
-            feature_results["name_match"].append(rng.random())
+        # feature_results["doc_id"] = []  # capture the doc id so we can join later
+        # feature_results["query_id"] = []  # ^^^
+        # feature_results["sku"] = []
+        # feature_results["salePrice"] = []
+        # feature_results["name_match"] = []
+        # rng = np.random.default_rng(12345)
+
+        try: self.opensearch.search(body=log_query, index=self.index_name)
+        except RequestError as re: print(re, log_query)
+        else:
+            if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
+                # we have a response with some hits
+                hits = response['hits']['hits']
+
+                for hit in hits:
+                    entry = hit['fields']['_ltrlog'][0]['log_entry']
+                    # {"name_match": 14.78, "salesPrice": 49.99 ....}
+                    d = {item['name']: item.get('value', None) for item in entry}
+
+                    # TODO for k in d.keys(): feature_results.get(k, []).append(d[key])
+                    feature_results.get('name_match', []).append(d['name_match'])
+                    feature_results.get('name_phrase_match', []).append(d['name_phrase_match'])
+                    feature_results.get('name_hyphens_min_df', []).append(d['name_hyphens_min_df'])
+                    feature_results.get('salePrice', []).append(d['salePrice'])
+                    feature_results.get('regularPrice', []).append(d['regularPrice'])
+                    
+                    feature_results.get('doc_id', []).append(hit['_id'])
+                    feature_results.get('query_id', []).append(query_id)
+                    feature_results.get('sku', []).append(hit['_source']['sku'][0])
+        
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
-        # IMPLEMENT_END
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
     def normalize_data(self, ranks_features_df, feature_set, normalize_type_map):
