fa1b03e60d28bfab51c26d211e6e7c43a3db462e
André Tzermias <andreportaro@gmail.com>
2022-02-25 21:55:40 +0000

    Implement step() and down_sampling

--- week2/utilities/click_models.py
+++ week2/utilities/click_models.py
@@ -8,8 +8,15 @@ def binary_func(x):
     return 0
 
 def step(x):
-    print("IMPLEMENT ME: step(x) a step function with a simple heuristic that buckets grades")
-    return rng.choice([0,0.5, 1.0])
+    if x < 0.05: 
+        return 0
+    elif x >= 0.05 and x < 0.10: 
+        return 0.5
+    elif x >= 0.10 and x < 0.3: 
+        return 0.75
+    else: 
+        return 1
+
 
 
 rng = np.random.default_rng(123456)
@@ -28,7 +35,9 @@ def apply_click_model(data_frame, click_model_type="binary", downsample=True):
             data_frame = down_sample_continuous(data_frame)
     elif click_model_type == "heuristic":
         data_frame["grade"] = (data_frame["clicks"]/data_frame["num_impressions"]).fillna(0).apply(lambda x: step(x))
-        print("IMPLEMENT ME: apply_click_model(): downsampling")
+        if downsample:
+            down_sample_buckets(data_frame)
+
     return data_frame
 
 # https://stackoverflow.com/questions/55119651/downsampling-for-more-than-2-classes
