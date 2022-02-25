792fccce22fc48d485bae5aa183c02c10a14852c
André Tzermias <andreportaro@gmail.com>
2022-02-25 01:45:20 +0000

    WIP trying to create feature dataframe

--- week2/utilities/data_prepper.py
+++ week2/utilities/data_prepper.py
@@ -234,24 +234,58 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         # IMPLEMENT_START --
         response = self.opensearch.search(log_query, "bbuy_products")
-        # print(response)
+        
         # print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
+        
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
-        feature_results["salePrice"] = []
+        # WIP 
         feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
-            feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  # ^^^
-            feature_results["salePrice"].append(rng.random())
-            feature_results["name_match"].append(rng.random())
+        feature_results["name_phrase_match"] = []
+        feature_results["name_hyphens_min_df"] = []
+        feature_results["salePrice"] = []
+        feature_results["regularPrice"] = []
+
+        for doc in response["hits"]["hits"]:
+            for log in doc['fields']['_ltrlog'][0]['log_entry']:
+                feature_results["doc_id"].append(doc["_id"])
+                feature_results["query_id"].append(query_id)
+                feature_results["sku"].append(doc["_id"])
+
+                name_match = np.nan
+                name_phrase_match = np.nan
+                name_hyphens_min_df = np.nan
+                salePrice = np.nan
+                regularPrice = np.nan
+
+                if "value" in log:
+                    value = log["value"]
+                else:
+                    value = None
+                
+                if log["name"] == "name_match":
+                    name_match = value
+                elif log["name"] == "name_phrase_match":
+                    name_phrase_match =value
+                elif log["name"] == "name_hyphens_min_df":
+                    name_hyphens_min_df = value
+                elif log["name"] == "salePrice":
+                    salePrice = value
+                elif log["name"] == "regularPrice":
+                    regularPrice = value
+
+                feature_results["name_match"].append(name_match)
+                feature_results["name_phrase_match"].append(name_phrase_match)
+                feature_results["name_hyphens_min_df"].append(name_hyphens_min_df)
+                feature_results["salePrice"].append(salePrice)
+                feature_results["regularPrice"].append(regularPrice)
+
         frame = pd.DataFrame(feature_results)
+        print(frame.head())
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
 
