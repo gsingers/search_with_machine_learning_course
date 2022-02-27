3b94db8f6b43825feff2302895ac6c4e1167e6c9
justinakiud <justina.kiudyte@vinted.com>
2022-02-27 20:43:50 +0000

    changes2

--- week2/utilities/query_utils.py
+++ week2/utilities/query_utils.py
@@ -18,7 +18,7 @@ def create_prior_queries_from_group(click_group): # total impressions isn't curr
     if click_group is not None:
         for item in click_group.itertuples():
             try:
-                click_prior_query += "%s^%.3f  " % (item.doc_id, item.clicks)
+                click_prior_query += "%s^%.3f  " % (item.doc_id, item.clicks/item.num_impressions)
 
             except KeyError as ke:
                 pass # nothing to do in this case, it just means we can't find priors for this doc
@@ -33,7 +33,7 @@ def create_prior_queries(doc_ids, doc_id_weights, query_times_seen): # total imp
         for idx, doc in enumerate(doc_ids):
             try:
                 wgt = doc_id_weights[doc]  # This should be the number of clicks or whatever
-                click_prior_query += "%s^%.3f  " % (doc, wgt)
+                click_prior_query += "%s^%.3f  " % (doc, wgt/query_times_seen)
             except KeyError as ke:
                 pass # nothing to do in this case, it just means we can't find priors for this doc
     return click_prior_query
