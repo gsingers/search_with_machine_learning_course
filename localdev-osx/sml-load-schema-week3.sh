37d8782ec9e2fb66e74f0187db97cb5d29c9832b
none <cody@telnet.org>
2022-02-28 21:24:25 -0600

    separate schema loading by week

---
+++ localdev-osx/sml-load-schema-week3.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#
+#
+
+
+echo
+echo "Loading products schema..."
+curl -k -X PUT -u ${ES_CREDS} "https://localhost:9200/bbuy_products" -H 'Content-Type: application/json' -d "@../week3/conf/bbuy_products.json"
+echo
+echo
+
+echo "Loading queries schema..."
+curl -k -X PUT -u ${ES_CREDS} "https://localhost:9200/bbuy_queries" -H 'Content-Type: application/json' -d "@../week3/conf/bbuy_queries.json"
+echo
+echo
+
+
