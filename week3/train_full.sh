329e435c3964c79dbdba0a1a4e4d44ea79506374
Dmitry Kan <dmitry.kan@gmail.com>
2022-03-06 21:37:25 +0000

    changes

--- week3/train_full.sh
+++ week3/train_full.sh
@@ -1,2 +1,3 @@
 #~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/fasttext/cats.train -output /workspace/datasets/fasttext/model.bin -loss hs -wordNgrams 2
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/fasttext/full_cats.train -output /workspace/datasets/fasttext/full_model_sample_punct_to_space_lc -loss hs -epoch 25 -lr 0.1
+
+~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/fasttext/full_cats_punct_to_space_lc.train -output /workspace/datasets/fasttext/full_model_sample_punct_to_space_lc -loss hs -epoch 25 -lr 0.1
