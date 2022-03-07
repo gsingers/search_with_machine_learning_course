083fa8de64ad2a80db18926e677e44f1c80406dd
Ovidiu Mihalcea <ovidiu@faction.ro>
2022-03-07 14:41:24 +0000

    Submit week3 project

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -1,11 +1,24 @@
 import argparse
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+import pandas
 import os
 import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
 def transform_name(product_name):
-    # IMPLEMENT
+    # lowercase
+    product_name = product_name.lower()
+    
+    # tokenize and strip punctuation
+    tokenizer = RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
+    tokens = tokenizer.tokenize(product_name)
+    
+    # snowball stemming
+    snowball = SnowballStemmer("english")
+    tokens = [snowball.stem(token) for token in tokens]
+    product_name = " ".join(tokens)
     return product_name
 
 # Directory for product data
@@ -19,7 +32,6 @@ general.add_argument("--output", default="/workspace/datasets/fasttext/output.fa
 # Consuming all of the product data will take over an hour! But we still want to be able to obtain a representative sample.
 general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
 
-# IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
 general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
 
 args = parser.parse_args()
@@ -31,9 +43,11 @@ if os.path.isdir(output_dir) == False:
 
 if args.input:
     directory = args.input
-# IMPLEMENT:  Track the number of items in each category and only output if above the min
+
 min_products = args.min_products
 sample_rate = args.sample_rate
+
+products_list = []
 
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
@@ -51,8 +65,24 @@ with open(output_file, 'w') as output:
                     child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
                     child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
                       # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
-                      name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+                    cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+                    # Replace newline chars with spaces so fastText doesn't complain
+                    name = child.find('name').text.replace('\n', ' ')
+                    # output.write("__label__%s %s\n" % (cat, transform_name(name)))
 
+                    product_assoc = {}
+                    product_assoc['category'] = cat
+                    product_assoc['name'] = transform_name(name)
+                    products_list.append(product_assoc)
+
+df = pandas.DataFrame(products_list)
+if min_products > 0:
+    print("Filtering min products per category results")
+    gb = df.groupby('category')
+    df_filtered = gb.filter(lambda x: x['category'].count() > min_products)
+    print("{} products filtered".format(len(df)-len(df_filtered)))
+    df = df_filtered
+
+with open(output_file, 'w') as output:
+    for index, row in df.iterrows():
+        output.write("__label__%s %s\n" % (row['category'], row['name']))
