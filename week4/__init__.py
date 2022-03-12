5be90ae75464551ce657fe885a55b4c9c735a1e3
olagustafsson1 <ola.gustafsson@mynewsdesk.com>
2022-03-12 17:56:22 +0000

    First changes to init

--- week4/__init__.py
+++ week4/__init__.py
@@ -5,14 +5,17 @@ from flask import render_template
 
 import fasttext
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-        QUERY_CLASS_MODEL_LOC = os.environ.get("QUERY_CLASS_MODEL_LOC", "/workspace/datasets/fasttext/query_model.bin")
+        app.config.from_pyfile("config.py", silent=True)
+        QUERY_CLASS_MODEL_LOC = os.environ.get(
+            "QUERY_CLASS_MODEL_LOC", "/workspace/models/categories.bin"
+        )
         if QUERY_CLASS_MODEL_LOC and os.path.isfile(QUERY_CLASS_MODEL_LOC):
             app.config["query_model"] = fasttext.load_model(QUERY_CLASS_MODEL_LOC)
         else:
@@ -23,7 +26,7 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     app.config["index_name"] = os.environ.get("INDEX_NAME", "bbuy_products")
-    
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -31,12 +34,13 @@ def create_app(test_config=None):
         pass
 
     # A simple landing page
-    #@app.route('/')
-    #def index():
+    # @app.route('/')
+    # def index():
     #    return render_template('index.jinja2')
 
     from . import search
+
     app.register_blueprint(search.bp)
-    app.add_url_rule('/', view_func=search.query)
+    app.add_url_rule("/", view_func=search.query)
 
     return app
