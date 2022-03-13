1f03e96b51c6f71d4e4b06b143c76869f8ec87eb
Atita Arora <atitaarora@gmail.com>
2022-03-13 12:46:38 +0000

    Draft 2 after rollup cats implemented

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -4,10 +4,15 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
-
 # Useful if you want to perform stemming.
 import nltk
-stemmer = nltk.stem.PorterStemmer()
+from nltk.stem import SnowballStemmer
+nltk.download("punkt")
+from nltk.tokenize import word_tokenize
+import string
+
+#stemmer = nltk.stem.PorterStemmer()
+stemmer = SnowballStemmer("english")
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
@@ -26,6 +31,19 @@ if args.min_queries:
     min_queries = int(args.min_queries)
 
 # IMPLEMENTED: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+#From week 3 exercise
+def transform_name(product_name):
+    # Transforming the name strings to lowercase, changing punctuation characters  
+    # to spaces, and stemming (you can use the NLTK Snowball stemmer)
+    spaceString=" "
+    product_name.lower()
+    for x in string.punctuation:
+        product_name=product_name.replace(x,spaceString)
+    stemmer = SnowballStemmer("english")
+    transformed_product_name = " ".join((stemmer.stem(w) for w in word_tokenize(product_name)))
+    return transformed_product_name
+
+#From Daniel T's solution
 def transform_query(query):
     # Transforming the name strings to lowercase, changing punctuation characters  
     # to spaces, and stemming (you can use the NLTK Snowball stemmer)
@@ -57,30 +75,32 @@ print("Data from Parent: {}\n",parents_df.head())
 
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 df = pd.read_csv(queries_file_name)[['category', 'query']]
-category_group = df['category'].value_counts()[lambda x: x>min_queries].index.tolist()
-#default code
-#df = df[df['category'].isin(categories)]
-df = df[df['category'].isin(category_group)]
-df['query']=df['query'].apply(transform_query)
+#deduce category groups to determine counts
+#category_group = df['category'].value_counts()[lambda x: x>min_queries].index.tolist()
+#df = df[df['category'].isin(category_group)]
 
-print("Data from Query:  {}\n",df.head())
+#default code
+df = df[df['category'].isin(categories)]
+df['transformed_query']=df['query'].apply(transform_query)
+
+print("Data from transformed Query:  {}\n",df.head())
 
 # count number of categories
 df["number_of_queries"] = df.groupby('category')['query'].transform(len)
-print("Number of queries:  {}\n",df.head())
 
-def get_parent_cat(query):
-    print("parent_df[category]: {}{}",parents_df['category'] , query)
-    return parents_df[parents_df['category']  == query]['parent'].values[0]
+# IMPLEMENTING: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+#Join the transformed query_df with parent_df to combine everything in one dataframe
+df = df.join(parents_df.set_index("category"), on="category")
 
-# get parent for each category
-df['parent'] = df['category'].apply(get_parent_cat) 
-print("Parent Category:  {}\n",df.head())
+df["parent_group"] = df.groupby("parent")["transformed_query"].transform(lambda x: len(set(x)))
+
+df["cat_group"] = df.groupby("category")["transformed_query"].transform(lambda x: len(set(x)))
+
+print("Data after grouping:  {}\n",df.head())
+
+df.loc[df["parent_group"] < min_queries, "drop"] = 1
 
 print("Unique categories : {}", df['category'].nunique())
-# IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
-
-# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
