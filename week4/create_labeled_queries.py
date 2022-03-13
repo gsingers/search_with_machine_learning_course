d64865307e469b84e50304766f2ae3f4680bfae2
Manikandan Sivanesan` <msivanes31@gmail.com>
2022-03-13 01:47:58 +0000

    transform queries and rollup categories to ancestors

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -4,10 +4,53 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+from nltk.stem import SnowballStemmer
+import re, string
 
 # Useful if you want to perform stemming.
 import nltk
-stemmer = nltk.stem.PorterStemmer()
+#stemmer = nltk.stem.PorterStemmer()
+
+stemmer = SnowballStemmer(language='english')
+_re_spec = re.compile(r'([/#\\-\\.:\'\"])')
+
+def spec_add_spaces(t):
+    "Add spaces around \" ' . : - / \ and #"
+    return _re_spec.sub(r' \1 ', t)
+
+# Causes the resulting RE to match from m to n repetitions of the preceding RE, attempting to match as many repetitions as possible.
+_re_space = re.compile(' {2,}')
+
+def rm_useless_spaces(t):
+    "Remove multiple spaces"
+    return _re_space.sub(' ', t)
+
+def rm_punct(t):
+    for p in string.punctuation:
+        t = t.replace(p, ' ')
+    return t
+
+def transform(query):
+    "Transform query by replacing punctuations, removing multiple spaces, stemming, lower casing"
+    
+    # replace_punct
+    query = spec_add_spaces(query)
+    
+    # remove punct
+    query = rm_punct(query)
+    
+    # replace multiple spaces
+    query = rm_useless_spaces(query)
+
+    # fix registered, trademark, copyright symbol
+    # remove non-ascii characters from query
+    query = query.encode(encoding='ascii', errors='ignore').decode()
+    
+    query = ' '.join([o for o in query.split(' ') if not o.isnumeric()])
+    
+    # add stemmer
+    query = stemmer.stem(query)
+    return query.lower()
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
@@ -48,9 +91,32 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 df = pd.read_csv(queries_file_name)[['category', 'query']]
 df = df[df['category'].isin(categories)]
 
-# IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+# IMPLEMENTED: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+df['query'] = df['query'].apply(transform)
 
-# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+def parent(cat):
+    if cat in parents_df.category.values:
+        return parents_df[parents_df.category == cat ].parent.to_list()[0]
+    else:
+        return root_category_id
+
+# IMPLEMENTED: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+cat_count = df.groupby('category').size().to_frame('count')
+
+print(f"# of Categories before pruning : {len(df.category.value_counts())}")
+
+# categories to be pruned
+prune_df = cat_count[cat_count['count'] < min_queries];
+
+while len(prune_df) > 0:
+    for cat, count in prune_df.iterrows():
+        df.replace(to_replace=cat, value=parent(cat), inplace=True)
+    cat_count = df.groupby('category').size().to_frame('count')
+    prune_df = cat_count[cat_count['count'] < min_queries]
+    print(f"Categories to be pruned : {len(prune_df)}")
+    
+print()
+print(f"# of Categories after pruning : {len(df.category.value_counts())}")
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
