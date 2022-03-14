a34917068c13831b27aa6645647197051fd43273
Max Gu <maxgu@Maxs-Mac-mini.local>
2022-03-14 09:58:52 -0700

    week3 project

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -1,12 +1,90 @@
 import argparse
+import collections
+import logging
 import os
 import random
+import string
+import unicodedata
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
+import nltk
+from nltk.stem.snowball import EnglishStemmer
+from nltk.tokenize import word_tokenize
+
+
+trans_table = str.maketrans('', '', string.punctuation)
+stemmer = EnglishStemmer(ignore_stopwords=True)
+logger = logging.getLogger(__name__)
+
+Product = collections.namedtuple("Product", ["category", "name"])
+
+
+def remove_punkt(tokens):
+    for token in tokens:
+        t = token.translate(trans_table)
+        if t:
+            yield t
+
+
+def stem(tokens):
+    yield from map(stemmer.stem, tokens)
+
+
+token_filters = [
+    remove_punkt,
+    stem
+]
+
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+    normalized = unicodedata.normalize("NFKD", product_name).encode("ascii", "ignore").decode("utf-8")
+    tokens = word_tokenize(normalized)
+    for tf in token_filters:
+        tokens = tf(tokens)
+    return " ".join(tokens)
+
+
+def iter_product_elems(directory):
+    for filename in os.listdir(directory):
+        logger.debug("Processing %s", filename)
+        if filename.endswith(".xml"):
+            filepath = os.path.join(directory, filename)
+            for _, elem in ET.iterparse(filepath):
+                if elem.tag == "product":
+                    yield elem
+
+
+def sample(iterable, rate):
+    assert 0 <= rate <= 1
+    if rate == 1:
+        yield from iterable
+    for elem in iterable:
+        if random.random() <= rate:
+            yield elem
+
+
+class ProductParser:
+    def __init__(self, max_category_depth):
+        self.max_category_depth = max_category_depth
+
+    def parse(self, product_elem):
+        category_path = product_elem.find("categoryPath")
+        category_path_idx = min(self.max_category_depth, len(category_path)-1)
+        return Product(
+            category=category_path[category_path_idx].find("id").text,
+            name=product_elem.find("name").text
+        )
+
+
+def filter_min_products(products, n):
+    if n == 0:
+        yield from products
+    product_list = list(products)
+    cnt = collections.Counter(product.category for product in product_list)
+    for product in product_list:
+        if cnt[product.category] >= n:
+            yield product
+
 
 # Directory for product data
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products/'
@@ -19,40 +97,28 @@ general.add_argument("--output", default="/workspace/datasets/fasttext/output.fa
 # Consuming all of the product data will take over an hour! But we still want to be able to obtain a representative sample.
 general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
 
-# IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
+# Setting min_products removes infrequent categories and makes the classifier's task easier.
 general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
 
-args = parser.parse_args()
-output_file = args.output
-path = Path(output_file)
-output_dir = path.parent
-if os.path.isdir(output_dir) == False:
-        os.mkdir(output_dir)
+general.add_argument("--max-category-depth", default=-1, type=int, help="Cap category depth to n (default is -1, which equals to no cap)")
 
-if args.input:
-    directory = args.input
-# IMPLEMENT:  Track the number of items in each category and only output if above the min
-min_products = args.min_products
-sample_rate = args.sample_rate
 
-print("Writing results to %s" % output_file)
-with open(output_file, 'w') as output:
-    for filename in os.listdir(directory):
-        if filename.endswith(".xml"):
-            print("Processing %s" % filename)
-            f = os.path.join(directory, filename)
-            tree = ET.parse(f)
-            root = tree.getroot()
-            for child in root:
-                if random.random() > sample_rate:
-                    continue
-                # Check to make sure category name is valid
-                if (child.find('name') is not None and child.find('name').text is not None and
-                    child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
-                    child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
-                      # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
-                      name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+if __name__ == "__main__":
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+    nltk.download(["punkt", "stopwords"])
 
+    args = parser.parse_args()
+    product_parser = ProductParser(args.max_category_depth)
+
+    products = map(product_parser.parse, sample(iter_product_elems(args.input), rate=args.sample_rate))
+    lines = [
+        f"__label__{product.category} {transform_name(product.name)}\n"
+        for product in filter_min_products(products, n=args.min_products)
+    ]
+    random.shuffle(lines)
+
+    path = Path(args.output)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    logger.info("Writing results to %s", args.output)
+    with path.open("w") as output:
+        output.writelines(lines)
--- week3/extractTitles.py
+++ week3/extractTitles.py
@@ -1,8 +1,35 @@
 import os
 import random
+import string
+import unicodedata
 import xml.etree.ElementTree as ET
 import argparse
 from pathlib import Path
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+
+trans_table = str.maketrans('', '', string.punctuation)
+lemmatizer = WordNetLemmatizer()
+
+
+def remove_punkt(tokens):
+    for token in tokens:
+        t = token.translate(trans_table)
+        if t:
+            yield t
+
+
+def lemmatize(tokens):
+    yield from map(lemmatizer.lemmatize, tokens)
+
+
+token_filters = [
+    remove_punkt,
+    lemmatize
+]
 
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products'
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -18,7 +45,7 @@ output_file = args.output
 path = Path(output_file)
 output_dir = path.parent
 if os.path.isdir(output_dir) == False:
-        os.mkdir(output_dir)
+    os.mkdir(output_dir)
 
 if args.input:
     directory = args.input
@@ -26,21 +53,26 @@ if args.input:
 sample_rate = args.sample_rate
 
 def transform_training_data(name):
-    # IMPLEMENT
-    return name.replace('\n', ' ')
+    normalized = unicodedata.normalize("NFKD", name).encode("ascii", "ignore").decode("utf-8")
+    tokens = word_tokenize(normalized.lower())
+    for tf in token_filters:
+        tokens = tf(tokens)
+    return " ".join(tokens)
 
 # Directory for product data
+if __name__ == "__main__":
+    nltk.download(["punkt", "stopwords", "wordnet", "omw-1.4"])
 
-print("Writing results to %s" % output_file)
-with open(output_file, 'w') as output:
-    for filename in os.listdir(directory):
-        if filename.endswith(".xml"):
-            f = os.path.join(directory, filename)
-            tree = ET.parse(f)
-            root = tree.getroot()
-            for child in root:
-                if random.random() > sample_rate:
-                    continue
-                if (child.find('name') is not None and child.find('name').text is not None):
-                    name = transform_training_data(child.find('name').text)
-                    output.write(name + "\n")
+    print("Writing results to %s" % output_file)
+    with open(output_file, 'w') as output:
+        for filename in os.listdir(directory):
+            if filename.endswith(".xml"):
+                f = os.path.join(directory, filename)
+                tree = ET.parse(f)
+                root = tree.getroot()
+                for child in root:
+                    if random.random() > sample_rate:
+                        continue
+                    if (child.find('name') is not None and child.find('name').text is not None):
+                        name = transform_training_data(child.find('name').text)
+                        output.write(name + "\n")
