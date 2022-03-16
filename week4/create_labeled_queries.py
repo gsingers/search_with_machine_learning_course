e92c03ff1be3b0220139921c76565140b5c55f55
Dmitry Kan <dmitry.kan@gmail.com>
2022-03-16 16:57:13 +0000

    added query normalization

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -4,10 +4,27 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+import re
 
 # Useful if you want to perform stemming.
 import nltk
 stemmer = nltk.stem.PorterStemmer()
+
+
+def normalize(queries):
+    normalized_queries = []
+    for query in queries:
+        clean = re.sub(r"[(),:.;@#?!&$/\"-]+\ *", " ", query)
+        clean = re.sub(r"[ ]+", " ", clean)
+        clean_lc = clean.lower()
+        stemmed = " ".join([stemmer.stem(word) for word in clean_lc.split()])
+        #if stemmed == "":
+        #    print("Empty stemmed sequence detected")
+        #    exit(1)
+        normalized_queries.append(stemmed.replace('\n', ' '))
+        #return clean_lc.replace('\n', ' ')
+    return normalized_queries
+    
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
@@ -24,6 +41,7 @@ output_file_name = args.output
 
 if args.min_queries:
     min_queries = int(args.min_queries)
+
 
 # The root category, named Best Buy with id cat00000, doesn't have a parent.
 root_category_id = 'cat00000'
@@ -43,12 +61,17 @@ for child in root:
         categories.append(leaf_id)
         parents.append(cat_path_ids[-2])
 parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 'parent'])
+print(f"parents_df={parents_df}")
 
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 df = pd.read_csv(queries_file_name)[['category', 'query']]
 df = df[df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+print(f"queries={df}")
+df["query"] = normalize(df["query"])
+print("After query normalization:")
+print(f"queries={df}")
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
 
