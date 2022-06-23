a39d5b012dcf966f4281e58efefbb52d024db4c5
Shweta Agrawal <shweta.agrawal@nykaa.com>
2022-06-23 10:47:20 +0000

    feature_logging_done

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -231,22 +231,39 @@ class DataPrepper:
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
+        response = None
+        try:
+            response = self.opensearch.search(log_query, self.index_name)
+        except RequestError as re:
+            print("Unable to get log query response: %s\t%s" % (log_query, re))
         # IMPLEMENT_START --
         print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
-        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
+        # Loop over the hits structure returned by running `log_query` and 
+        #then extract out the features from the response per query_id and doc id.  
+        #Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
+        es_data = {'sku': [], 'name_match': []}
+        if response and response["hits"]["hits"]:
+            hits = response["hits"]["hits"]
+            for hit in hits:
+                es_data['sku'].append(hit["_id"])
+                log_data = hit["fields"]['_ltrlog'][0]['log_entry'][0]
+                es_data['name_match'].append(log_data.get('value', 0))
+        feature_val_df = pd.DataFrame(es_data)
+        
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
+        #feature_results["name_match"] = []
+        #rng = np.random.default_rng(12345)
         for doc_id in query_doc_ids:
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            #feature_results["name_match"].append(rng.random())
         frame = pd.DataFrame(feature_results)
+        frame = pd.merge(frame.astype({'sku': 'int64'}), feature_val_df.astype({'sku': 'int64'}), on='sku', how="inner")
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
 
--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -58,7 +58,37 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
     print("IMPLEMENT ME: create_feature_log_query")
-    return None
+    feature_log_query = {
+        "size": size,
+        "query": {
+            "bool": {
+            "filter": [
+                {
+                "terms": {
+                        terms_field : doc_ids
+                }},
+                {
+                "sltr": {
+                "_name": "logged_featureset",
+                "featureset": featureset_name,
+                "store": ltr_store_name,
+                "params": {
+                    "keywords": query
+                }
+                }}
+            ]
+            }
+        },
+        "ext": {
+            "ltr_log": {
+                "log_specs": {
+                    "name": "log_entry",
+                    "named_query": "logged_featureset"
+                }
+            }
+        }
+    }
+    return feature_log_query
 
 
 # Item is a Pandas namedtuple
