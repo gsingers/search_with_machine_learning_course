92d35a6ff73ee3a6a4b19365d78d5aeb61efc51d
Fraser Xu <xvfeng123@gmail.com>
2022-06-23 00:48:02 +0000

    setup train params

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -57,7 +57,17 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
+    # print("IMPLEMENT ME: create_feature_log_query")
+    sltr = {
+        "sltr": {
+            "_name": "logged_featureset",
+            "featureset": featureset_name,
+            "store": ltr_store_name,
+            "params": {
+                "keywords": query
+            }
+        }
+    }
     return None
 
 
