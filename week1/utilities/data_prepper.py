0874dc8da8c3ada22a21699ccebdd334ed5ee659
Shweta Agrawal <shweta.agrawal@nykaa.com>
2022-06-23 17:58:10 +0000

    recoring

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -237,7 +237,7 @@ class DataPrepper:
         except RequestError as re:
             print("Unable to get log query response: %s\t%s" % (log_query, re))
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+        #print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and 
         #then extract out the features from the response per query_id and doc id.  
         #Also capture and return all query/doc pairs that didn't return features
--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -10,7 +10,23 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
     # Create the base query, use a much bigger window
     #add on the rescore
     ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    #print("IMPLEMENT ME: create_rescore_ltr_query")
+    query_obj["rescore"] =  {
+    "window_size": rescore_size,
+    "query": {
+        "rescore_query": {
+            "sltr": {
+                "params": {
+                    "keywords": user_query
+                },
+                "model": ltr_model_name,
+                "store": ltr_store_name
+            }
+        },
+        "rescore_query_weight": rescore_query_weight,
+        "query_weight": main_query_weight
+    }
+}
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
@@ -57,7 +73,7 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
+    #print("IMPLEMENT ME: create_feature_log_query")
     feature_log_query = {
         "size": size,
         "query": {
