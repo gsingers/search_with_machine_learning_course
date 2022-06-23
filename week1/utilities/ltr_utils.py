5d6dd18d66dd01eb6249d7e1f2c177dad309e876
Tim Holland <th0114nd@gmail.com>
2022-06-22 20:45:03 -0400

    log and rescore queries

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -10,7 +10,23 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
     # Create the base query, use a much bigger window
     #add on the rescore
     ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    assert 'rescore' not in query_obj
+    query_obj['rescore'] = {
+        'window_size': rescore_size,
+        'query': {
+            'rescore_query': {
+                'sltr': {
+                    'params': {
+                        'keywords': user_query,
+                    },
+                    'model': ltr_model_name,
+                    'store': ltr_store_name,
+                }
+            },
+            'query_weight': main_query_weight,
+            'rescore_query_weight': rescore_query_weight,
+        }
+    }
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
@@ -57,8 +73,40 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
-    return None
+    return {
+        'query': {
+            'bool': {
+                'filter': [
+                    {
+                        'terms': {
+                            terms_field: doc_ids,
+                        }
+                    },
+                    {
+                        'sltr': {
+                            '_name': 'create_feature_log_query',
+                            'featureset': featureset_name,
+                            'store': ltr_store_name,
+                            'params': {
+                                'keywords': query,
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        'size': size,
+        'ext': {
+            'ltr_log': {
+                'log_specs': {
+                    'name': 'log_entry',
+                    'named_query': 'create_feature_log_query',
+                },
+            },
+        },
+    }
+
+
 
 
 # Item is a Pandas namedtuple
