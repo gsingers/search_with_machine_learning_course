991fa09420959b8fc269e1f31a503f505eef4aa0
GUIDO <guidolo@gmail.com>
2022-06-24 23:03:11 +0000

    Finish exercise 2

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -254,19 +254,28 @@ class DataPrepper:
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
+        feature_results["customerReviewAverage"] = []
+        feature_results["customerReviewCount"] = []
         #rng = np.random.default_rng(12345)
         for doc_id in query_doc_ids:
         #for hit in hits:
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)
-            value = 0
+            name_value = 0
+            customerReviewAverage_value = 0
+            customerReviewCount_value = 0
             for hit in hits:
                 if str(doc_id) == hit['_id']:
-                    value = hit['fields']['_ltrlog'][0]['log_entry'][0].get('value', 0)
-                    
+                    # I am assumming that features always comes in the same place
+                    # Maybe I am wrong but it is something that I could be improve
+                    name_value = hit['fields']['_ltrlog'][0]['log_entry'][0].get('value', 0)
+                    customerReviewAverage_value = hit['fields']['_ltrlog'][0]['log_entry'][1].get('value', 0)
+                    customerReviewCount_value = hit['fields']['_ltrlog'][0]['log_entry'][2].get('value', 0)
             #print(str(doc_id), str(hit['_id']), value)
-            feature_results["name_match"].append(value)
+            feature_results["name_match"].append(name_value)
+            feature_results["customerReviewAverage"].append(customerReviewAverage_value)
+            feature_results["customerReviewCount"].append(customerReviewCount_value)
 
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
