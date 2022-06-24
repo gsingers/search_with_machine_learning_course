3d1013736964f1b77d34463af299b7846644ea96
Fraser Xu <xvfeng123@gmail.com>
2022-06-24 23:57:01 +0000

    feature logging

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -58,17 +58,39 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
     # print("IMPLEMENT ME: create_feature_log_query")
-    sltr = {
-        "sltr": {
-            "_name": "logged_featureset",
-            "featureset": featureset_name,
-            "store": ltr_store_name,
-            "params": {
-                "keywords": query
+    query_obj = {
+            'query': {
+                'bool': {
+                    'filter': [
+                        {
+                            'terms': {
+                                terms_field: doc_ids
+                            }
+                        },
+                        {
+                            'sltr': {
+                                '_name': 'logged_featureset',
+                                'featureset': featureset_name,
+                                'store': ltr_store_name,
+                                'params': {
+                                    'keywords': query
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            'ext': {
+                'ltr_log': {
+                    'log_specs': {
+                        'name': 'log_entry',
+                        'named_query': 'logged_featureset'
+                    }
+                }
             }
         }
-    }
-    return None
+
+    return query_obj
 
 
 # Item is a Pandas namedtuple
