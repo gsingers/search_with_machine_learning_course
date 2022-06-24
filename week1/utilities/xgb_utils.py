a38ced24c2e6058a18406eb3236d4c23a120de18
Rocky Chen <rthenc@icloud.com>
2022-06-24 18:28:35 +0000

    step 3: implement train method in xgb_utils.py

--- week1/utilities/xgb_utils.py
+++ week1/utilities/xgb_utils.py
@@ -34,4 +34,9 @@ def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
     print("Training XG Boost on %s for %s rounds with params: %s" % (xgb_train_data, num_rounds, xgb_params))
     ##### Step 3.a
     print("IMPLEMENT ME: train()")
+    bst = xgb.train(
+        params=xgb_params,
+        dtrain=xgb_train_data,
+        num_boost_round=num_rounds,
+    )
     return bst, xgb_params
