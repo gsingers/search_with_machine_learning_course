b1bfaa37e9a41ed021e11f763c9fff3352a0442f
Natasha <notifynatasha@gmail.com>
2022-06-24 21:05:36 +0000

    level 1 progress

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -234,23 +234,27 @@ class DataPrepper:
         # IMPLEMENT_START --
         # print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
-        # hits = log_query["hits"]["hits"]
+        response = self.opensearch.search(body=log_query, index=self.index_name)
+        hits = response["hits"]["hits"]
         # Your structure should look like the data frame below
-        # hit["_id"] for hit in hits
-        # hit["sku"] for hit in hits
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
         rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
+        for e, doc_id in enumerate(query_doc_ids):
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            # feature_results["name_match"].append(rng.random())
+            try:
+                log_entry = hits[e]["fields"]["_ltrlog"][0]["log_entry"][0]
+                feature_results["name_match"].append(log_entry.get("value", 0))
+            except:
+                feature_results["name_match"].append(0)
         frame = pd.DataFrame(feature_results)
-        return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
+        return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64', 'name_match': 'float'})
         # IMPLEMENT_END
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -10,7 +10,24 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
     # Create the base query, use a much bigger window
     #add on the rescore
     ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    # print("IMPLEMENT ME: create_rescore_ltr_query")
+    query_obj["rescore"] = {
+        "window_size": rescore_size,
+        "query":{
+            "rescore_query": {
+                "sltr": {
+                    "params":{
+                        "keywords": user_query,
+                    },
+                "model": ltr_model_name,
+                "store": ltr_store_name,
+                },
+            },
+            "query_weight": main_query_weight,
+            "rescore_query_weight": rescore_query_weight
+        }
+    }
+
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
@@ -57,7 +74,7 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
+    # print("IMPLEMENT ME: create_feature_log_query")
     """
     Step 3.b: We need to log our features, so implement create_feature_log_query() in ltr_utils.py
     by creating a feature logging query (e.g. use the SLTR query with the ext setting) that can
@@ -74,10 +91,10 @@ def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name,
     featureset_name = "bbuy_main_featureset"
     index_name="bbuy_products"
     ltr_store_name="week1"
-    query= "ipad"
-    doc_ids=["5879736", "2678269"]
+    query= "zune"
+    doc_ids=[9448216, 9448234, 9956028]
     size=5
-    terms_field="_id"
+    terms_field="sku"
 
     """
     """
@@ -114,7 +131,7 @@ def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name,
                 "filter": [  # use a filter so that we don't actually score anything
                     {
                         "terms": {
-                            terms_field: [doc_ids]
+                            terms_field: doc_ids
                         }
                     },
                     {  # use the LTR query bring in the LTR feature set
