b5b368e12bc16d9ac604a2056e9e45c51a9f4535
Rocky Chen <rthenc@icloud.com>
2022-06-26 21:21:33 +0000

    rm print implement me

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -245,7 +245,6 @@ class DataPrepper:
 
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         response = self.opensearch.search(body=log_query, index=self.index_name)
 
