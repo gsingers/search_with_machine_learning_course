ffdc43cec529a56124c30f1b89f580400855a189
Rocky Chen <rthenc@icloud.com>
2022-06-26 21:47:54 +0000

    add total score_mode to create_rescore_ltr_query?

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -23,6 +23,7 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
                     "store": ltr_store_name,
                 }
             },
+            "score_mode": "total",
             "query_weight": main_query_weight,
             "rescore_query_weight": rescore_query_weight # Magic number, but let's say LTR matches are 2x baseline matches
         }
