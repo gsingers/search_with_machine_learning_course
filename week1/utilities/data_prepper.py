5809b5a46ce7bcb378911d19c9e2c5df46af01b9
marcospfernandes <marcospqf@gmail.com>
2022-06-27 04:59:35 +0000

    Finishing Level 1

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -230,22 +230,31 @@ class DataPrepper:
         log_query = lu.create_feature_log_query(key, query_doc_ids, click_prior_query, self.featureset_name,
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
+
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+     
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
+        response = self.opensearch.search(log_query, self.index_name)
+        searched_documents = { hit["_source"][terms_field][0]: hit for hit in response["hits"]["hits"] }
+
+        for doc_id, doc_stats in searched_documents.items():
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+
+            extra_features = map(lambda feature: (feature["name"], feature.get("value", 0.0)),doc_stats["fields"]["_ltrlog"][0]["log_entry"])
+            for feature, value in extra_features:
+                if feature not in feature_results.keys():
+                    feature_results[feature] = []
+                
+                feature_results[feature].append(value)
+
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -9,8 +9,24 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
                              rescore_size=500, main_query_weight=1, rescore_query_weight=2):
     # Create the base query, use a much bigger window
     #add on the rescore
-    ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    ##### Step 4.e:        
+    query_obj["rescore"] = {
+        "window_size": rescore_size,
+        "query": {
+            "rescore_query": {
+                "sltr": {
+                    "params": {
+                        "keywords": user_query
+                    },
+                    "model": ltr_model_name,
+                    "store": ltr_store_name,
+                }
+            },
+            "rescore_query_weight": rescore_query_weight,
+            "query_weight": main_query_weight
+        }
+    }
+
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
@@ -27,10 +43,10 @@ def create_sltr_simple_query(user_query, query_obj, click_prior_query, ltr_model
                 "click_prior_query": click_prior_query
             },
             "model": ltr_model_name,
-            # Since we are using a named store, as opposed to simply '_ltr', we need to pass it in
             "store": ltr_store_name,
         }
     }
+
     if active_features is not None and len(active_features) > 0:
         sltr["active_features"] =  active_features
     query_obj["query"]["bool"]["should"].append(sltr)
@@ -57,8 +73,40 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
-    return None
+    query_obj = {
+        'size': size,
+        'query': {
+            'bool': {
+                'filter': [
+                    {
+                        'terms': {
+                            terms_field: doc_ids
+                        }
+                    },
+                    {
+                        'stlr': {
+                            '_name': "log_feature_set",
+                            'feature_set': featureset_name,
+                            'store': ltr_store_name,
+                            'params': {
+                                'keywords': query
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        'ext': {
+            'ltr_log': {
+                'log_specs': {
+                    'name': 'log_entry',
+                    'named_query': ltr_store_name,
+                }
+            }
+        }
+    }
+    
+    return query_obj
 
 
 # Item is a Pandas namedtuple
