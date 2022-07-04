3c7a99ae756fed4143fe90880f6ab13a10c4b854
gsingers <gsingers@apache.org>
2022-07-04 09:49:46 -0400

    minor cleanup

--- utilities/index_products.py
+++ utilities/index_products.py
@@ -104,22 +104,6 @@ def get_opensearch():
     )
     return client
 
-def annotate_document(doc, doc_url):
-    #payload = json.dumps(doc)
-    payload = json.dumps(doc)
-    #logger.info(f"Sending doc: {payload} to {doc_url}")
-    headers = {
-      'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", doc_url, headers=headers, data=payload)
-    logger.info(f"Annotation response: {response.status_code}")
-    name_syns = response.json()
-    if name_syns is not None and len(name_syns) > 0 and name_syns["name_synonyms"] is not None:
-        doc["name_synonyms"] = name_syns["name_synonyms"]
-        logger.info(f"Adding: {name_syns} to document: {doc}")
-    else:
-        logger.info(f"Invalid synonyms response: {name_syns}")
-
 
 def index_file(file, index_name, synonyms=False, documents_url="http://localhost:5000/documents/annotate", reduced=False):
     docs_indexed = 0
@@ -160,15 +144,14 @@ def index_file(file, index_name, synonyms=False, documents_url="http://localhost
 @click.option('--index_name', '-i', default="bbuy_products", help="The name of the index to write to")
 @click.option('--workers', '-w', default=8, help="The name of the index to write to")
 @click.option('--documents_url', '-d', default="http://localhost:5000/documents/annotate", help="The location of the Flask App endpoint, something like http://localhost:5000/documents/annotate")
-@click.option('--synonyms', is_flag=True, show_default=True, default=False, help="If true, add synonyms to the document using the --documents_url endpoint.")
 @click.option('--reduced', is_flag=True, show_default=True, default=False, help="Removes music, movies, and merchandised products.")
-def main(source_dir: str, index_name: str, reduced: bool, workers: int, synonyms: bool, documents_url: str):
-    logger.info(f"Indexing {source_dir} to {index_name} with {workers} workers, synonyms is {synonyms} and the reduced flag set to {reduced}.  Documents URL is {documents_url}")
+def main(source_dir: str, index_name: str, reduced: bool, workers: int, documents_url: str):
+    logger.info(f"Indexing {source_dir} to {index_name} with {workers} workers, the reduced flag set to {reduced}.  Documents URL is {documents_url}")
     files = glob.glob(source_dir + "/*.xml")
     docs_indexed = 0
     start = perf_counter()
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
-        futures = [executor.submit(index_file, file, index_name, synonyms, documents_url, reduced) for file in files]
+        futures = [executor.submit(index_file, file, index_name, documents_url, reduced) for file in files]
         for future in concurrent.futures.as_completed(futures):
             docs_indexed += future.result()
 
--- utilities/query.py
+++ utilities/query.py
@@ -91,7 +91,7 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                                     "minimum_should_match": "2<75%",
                                     "fields": ["name^10", "name.hyphens^10", "shortDescription^5",
                                                "longDescription^5", "department^0.5", "sku", "manufacturer", "features",
-                                               "categoryPath", "name_synonyms"]
+                                               "categoryPath"]
                                 }
                             },
                             {
