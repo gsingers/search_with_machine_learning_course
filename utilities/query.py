8f47622ee878ebf44ca609401934a65884b15a51
sams-git <mittal.sameer@gmail.com>
2022-07-06 07:35:04 +0000

    synonyms search

--- utilities/query.py
+++ utilities/query.py
@@ -1,31 +1,37 @@
 # A simple client for querying driven by user input on the command line.  Has hooks for the various
 # weeks (e.g. query understanding).  See the main section at the bottom of the file
-from opensearchpy import OpenSearch
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
 import argparse
+
+# import fileinput
 import json
+import logging
 import os
+import warnings
 from getpass import getpass
 from urllib.parse import urljoin
-import pandas as pd
-import fileinput
-import logging
 
+import pandas as pd
+from opensearchpy import OpenSearch
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logging.basicConfig(format='%(levelname)s:%(message)s')
+logging.basicConfig(format="%(levelname)s:%(message)s")
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
-        click_group):  # total impressions isn't currently used, but it mayb worthwhile at some point
+    click_group,
+):  # total impressions isn't currently used, but it mayb worthwhile at some point
     click_prior_query = ""
     # Create a string that looks like:  "query": "1065813^100 OR 8371111^89", where the left side is the doc id and the right side is the weight.  In our case, the number of clicks a document received in the training set
     if click_group is not None:
         for item in click_group.itertuples():
             try:
-                click_prior_query += "%s^%.3f  " % (item.doc_id, item.clicks / item.num_impressions)
+                click_prior_query += "%s^%.3f  " % (
+                    item.doc_id,
+                    item.clicks / item.num_impressions,
+                )
 
             except KeyError as ke:
                 pass  # nothing to do in this case, it just means we can't find priors for this doc
@@ -33,49 +39,63 @@ def create_prior_queries_from_group(
 
 
 # expects clicks from the raw click logs, so value_counts() are being passed in
-def create_prior_queries(doc_ids, doc_id_weights,
-                         query_times_seen):  # total impressions isn't currently used, but it mayb worthwhile at some point
+def create_prior_queries(
+    doc_ids, doc_id_weights, query_times_seen
+):  # total impressions isn't currently used, but it mayb worthwhile at some point
     click_prior_query = ""
     # Create a string that looks like:  "query": "1065813^100 OR 8371111^89", where the left side is the doc id and the right side is the weight.  In our case, the number of clicks a document received in the training set
     click_prior_map = ""  # looks like: '1065813':100, '8371111':809
     if doc_ids is not None and doc_id_weights is not None:
         for idx, doc in enumerate(doc_ids):
             try:
-                wgt = doc_id_weights[doc]  # This should be the number of clicks or whatever
-                click_prior_query += "%s^%.3f  " % (doc, wgt / query_times_seen)
+                wgt = doc_id_weights[
+                    doc
+                ]  # This should be the number of clicks or whatever
+                click_prior_query += "%s^%.3f  " % (
+                    doc,
+                    wgt / query_times_seen,
+                )
             except KeyError as ke:
                 pass  # nothing to do in this case, it just means we can't find priors for this doc
     return click_prior_query
 
 
 # Hardcoded query here.  Better to use search templates or other query config.
-def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None, use_synonyms=False):
+def create_query(
+    user_query,
+    click_prior_query,
+    filters,
+    sort="_score",
+    sortDir="desc",
+    size=10,
+    source=None,
+    use_synonyms: bool = False,
+):
 
-    match_name = "name"
     if use_synonyms:
-        match_name = "name.synonyms"
+        name_field: str = "name.synonyms"
+    else:
+        name_field = "name"
 
     query_obj = {
-        'size': size,
-        "sort": [
-            {sort: {"order": sortDir}}
-        ],
+        "size": size,
+        "sort": [{sort: {"order": sortDir}}],
         "query": {
             "function_score": {
                 "query": {
                     "bool": {
-                        "must": [
-
-                        ],
+                        "must": [],
                         "should": [  #
                             {
                                 "match": {
-                                    match_name: {
+                                    name_field: {
                                         "query": user_query,
                                         "fuzziness": "1",
                                         "prefix_length": 2,
-                                        # short words are often acronyms or usually not misspelled, so don't edit
-                                        "boost": 0.01
+                                        # short words are often acronyms
+                                        #   or usually not misspelled,
+                                        #   so don't edit
+                                        "boost": 0.01,
                                     }
                                 }
                             },
@@ -84,7 +104,7 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                                     "name.hyphens": {
                                         "query": user_query,
                                         "slop": 1,
-                                        "boost": 50
+                                        "boost": 50,
                                     }
                                 }
                             },
@@ -94,16 +114,25 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                                     "type": "phrase",
                                     "slop": "6",
                                     "minimum_should_match": "2<75%",
-                                    "fields": ["name^10", "name.hyphens^10", "shortDescription^5",
-                                               "longDescription^5", "department^0.5", "sku", "manufacturer", "features",
-                                               "categoryPath", "name_synonyms"]
+                                    "fields": [
+                                        f"{name_field}^10",
+                                        "name.hyphens^10",
+                                        "shortDescription^5",
+                                        "longDescription^5",
+                                        "department^0.5",
+                                        "sku",
+                                        "manufacturer",
+                                        "features",
+                                        "categoryPath",
+                                        "name_synonyms",
+                                    ],
                                 }
                             },
                             {
                                 "terms": {
                                     # Lots of SKUs in the query logs, boost by it, split on whitespace so we get a list
                                     "sku": user_query.split(),
-                                    "boost": 50.0
+                                    "boost": 50.0,
                                 }
                             },
                             {  # lots of products have hyphens in them or other weird casing things like iPad
@@ -111,75 +140,61 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                                     "name.hyphens": {
                                         "query": user_query,
                                         "operator": "OR",
-                                        "minimum_should_match": "2<75%"
+                                        "minimum_should_match": "2<75%",
                                     }
                                 }
-                            }
+                            },
                         ],
                         "minimum_should_match": 1,
-                        "filter": filters  #
+                        "filter": filters,  #
                     }
                 },
                 "boost_mode": "multiply",  # how _score and functions are combined
                 "score_mode": "sum",  # how functions are combined
                 "functions": [
                     {
-                        "filter": {
-                            "exists": {
-                                "field": "salesRankShortTerm"
-                            }
-                        },
+                        "filter": {"exists": {"field": "salesRankShortTerm"}},
                         "gauss": {
                             "salesRankShortTerm": {
                                 "origin": "1.0",
-                                "scale": "100"
-                            }
-                        }
-                    },
-                    {
-                        "filter": {
-                            "exists": {
-                                "field": "salesRankMediumTerm"
+                                "scale": "100",
                             }
                         },
+                    },
+                    {
+                        "filter": {"exists": {"field": "salesRankMediumTerm"}},
                         "gauss": {
                             "salesRankMediumTerm": {
                                 "origin": "1.0",
-                                "scale": "1000"
-                            }
-                        }
-                    },
-                    {
-                        "filter": {
-                            "exists": {
-                                "field": "salesRankLongTerm"
+                                "scale": "1000",
                             }
                         },
+                    },
+                    {
+                        "filter": {"exists": {"field": "salesRankLongTerm"}},
                         "gauss": {
                             "salesRankLongTerm": {
                                 "origin": "1.0",
-                                "scale": "1000"
+                                "scale": "1000",
                             }
-                        }
+                        },
                     },
-                    {
-                        "script_score": {
-                            "script": "0.0001"
-                        }
-                    }
-                ]
-
+                    {"script_score": {"script": "0.0001"}},
+                ],
             }
-        }
+        },
     }
+    # print(json.dumps(query_obj, indent=4))
     if click_prior_query is not None and click_prior_query != "":
-        query_obj["query"]["function_score"]["query"]["bool"]["should"].append({
-            "query_string": {
-                # This may feel like cheating, but it's really not, esp. in ecommerce where you have all this prior data,  You just can't let the test clicks leak in, which is why we split on date
-                "query": click_prior_query,
-                "fields": ["_id"]
+        query_obj["query"]["function_score"]["query"]["bool"]["should"].append(
+            {
+                "query_string": {
+                    # This may feel like cheating, but it's really not, esp. in ecommerce where you have all this prior data,  You just can't let the test clicks leak in, which is why we split on date
+                    "query": click_prior_query,
+                    "fields": ["_id"],
+                }
             }
-        })
+        )
     if user_query == "*" or user_query == "#":
         # replace the bool
         try:
@@ -191,41 +206,82 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort=None, sortDir="desc"):
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"],use_synonyms=use_synonyms)
+def search(
+    client,
+    user_query,
+    index="bbuy_products",
+    sort="_score",
+    sortDir="desc",
+    use_synonyms: bool = False,
+):
+    #### W3: classify the query
+    #### W3: create filters and boosts
+    # Note: you may also want to modify the `create_query` method above
+    query_obj = create_query(
+        user_query,
+        click_prior_query=None,
+        filters=None,
+        sort=sort,
+        sortDir=sortDir,
+        source=["name", "shortDescription"],
+        use_synonyms=use_synonyms,
+    )
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
-    if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
-        hits = response['hits']['hits']
+    if (
+        response
+        and response["hits"]["hits"]
+        and len(response["hits"]["hits"]) > 0
+    ):
+        hits = response["hits"]["hits"]
         print(json.dumps(response, indent=2))
 
 
 if __name__ == "__main__":
-    host = 'localhost'
+    host = "localhost"
     port = 9200
-    auth = ('admin', 'admin')  # For testing only. Don't store credentials in code.
-    parser = argparse.ArgumentParser(description='Build LTR.')
+    auth = (
+        "admin",
+        "admin",
+    )  # For testing only. Don't store credentials in code.
+    parser = argparse.ArgumentParser(description="Build LTR.")
     general = parser.add_argument_group("general")
-    general.add_argument("-i", '--index', default="bbuy_products",
-                         help='The name of the main index to search')
-    general.add_argument("-s", '--host', default="localhost",
-                         help='The OpenSearch host name')
-    general.add_argument("-p", '--port', type=int, default=9200,
-                         help='The OpenSearch port')
-    general.add_argument('--user',
-                         help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
-
-    general.add_argument('--synonyms',
-                         help='Adds synonyms to your search'
-                         ,dest='use_synonyms', default=False
-                         ,action='store_true'
-                         )
+    general.add_argument(
+        "-i",
+        "--index",
+        default="bbuy_products",
+        help="The name of the main index to search",
+    )
+    general.add_argument(
+        "-s", "--host", default="localhost", help="The OpenSearch host name"
+    )
+    general.add_argument(
+        "-p", "--port", type=int, default=9200, help="The OpenSearch port"
+    )
+    general.add_argument(
+        "--user",
+        help="The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin",
+    )
+    general.add_argument(
+        "--synonyms",
+        action="store_true",
+        help=(
+            "synonyms option. "
+            "If this is set, name.synonyms will replace name in search query"
+        ),
+    )
 
     args = parser.parse_args()
 
     if len(vars(args)) == 0:
         parser.print_usage()
         exit()
+
+    if bool(args.synonyms):
+        print(
+            f"synonyms == {args.synonyms} is requested; "
+            "use ``name.synonyms`` in search query"
+        )
 
     host = args.host
     port = args.port
@@ -235,7 +291,7 @@ if __name__ == "__main__":
 
     base_url = "https://{}:{}/".format(host, port)
     opensearch = OpenSearch(
-        hosts=[{'host': host, 'port': port}],
+        hosts=[{"host": host, "port": port}],
         http_compress=True,  # enables gzip compression for request bodies
         http_auth=auth,
         # client_cert = client_cert_path,
@@ -244,18 +300,24 @@ if __name__ == "__main__":
         verify_certs=False,  # set to true if you have certs
         ssl_assert_hostname=False,
         ssl_show_warn=False,
-
     )
     index_name = args.index
+    use_synonyms: bool = args.synonyms
     query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
-    print(query_prompt)
-    for line in fileinput.input():
-        query = line.rstrip()
-        if query == "Exit":
+    while True:
+        try:
+            query: str = str(input(query_prompt)).rstrip()
+        except KeyboardInterrupt:
             break
-        #### W3: classify the query
-        search(client=opensearch, user_query=query, index=index_name)
+        else:
+            if query.lower() == "exit":
+                break
+            else:
+                search(
+                    client=opensearch,
+                    user_query=query,
+                    index=index_name,
+                    use_synonyms=use_synonyms,
+                )
 
-        print(query_prompt)
-
-    
+ 
--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -3,10 +3,11 @@ import os
 import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from nltk.stem.snowball import EnglishStemmer
 
 def transform_name(product_name):
     # IMPLEMENT
-    return product_name
+    return EnglishStemmer().stem(product_name)
 
 # Directory for product data
 directory = r'/workspace/datasets/product_data/products/'
