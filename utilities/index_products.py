d297dc5872cf2dbe1e2a90aebeb48d4ebd545a8d
gsingers <gsingers@apache.org>
2022-07-09 15:39:00 -0400

    week 4 changes

--- utilities/index_products.py
+++ utilities/index_products.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import requests
 import json
 
+### W4: S1: Import the sentence transformer library.  Note: you may need to pip install it as we've noticed it doesn't always get installed properly despite being in our requirements.txt
+
 from time import perf_counter
 import concurrent.futures
 
@@ -105,8 +107,9 @@ def get_opensearch():
     return client
 
 
-def index_file(file, index_name, synonyms=False, documents_url="http://localhost:5000/documents/annotate", reduced=False):
+def index_file(file, index_name, reduced=False):
     docs_indexed = 0
+    ### W4: S1: Load the model.  # We do this here to avoid threading issues
     client = get_opensearch()
     logger.info(f'Processing file : {file}')
     tree = etree.parse(file)
@@ -124,9 +127,7 @@ def index_file(file, index_name, synonyms=False, documents_url="http://localhost
             continue
         if reduced and ('categoryPath' not in doc or 'Best Buy' not in doc['categoryPath'] or 'Movies & Music' in doc['categoryPath']):
             continue
-        if synonyms:
-            annotate_document(doc, documents_url)
-            logger.info(f"Added Syns: {doc}")
+        ### W4: S2: Encode the names
         docs.append({'_index': index_name, '_id':doc['sku'][0], '_source' : doc})
         #docs.append({'_index': index_name, '_source': doc})
         docs_indexed += 1
@@ -143,15 +144,14 @@ def index_file(file, index_name, synonyms=False, documents_url="http://localhost
 @click.option('--source_dir', '-s', help='XML files source directory')
 @click.option('--index_name', '-i', default="bbuy_products", help="The name of the index to write to")
 @click.option('--workers', '-w', default=8, help="The name of the index to write to")
-@click.option('--documents_url', '-d', default="http://localhost:5000/documents/annotate", help="The location of the Flask App endpoint, something like http://localhost:5000/documents/annotate")
 @click.option('--reduced', is_flag=True, show_default=True, default=False, help="Removes music, movies, and merchandised products.")
 def main(source_dir: str, index_name: str, reduced: bool, workers: int, documents_url: str):
-    logger.info(f"Indexing {source_dir} to {index_name} with {workers} workers, the reduced flag set to {reduced}.  Documents URL is {documents_url}")
+    logger.info(f"Indexing {source_dir} to {index_name} with {workers} workers, the reduced flag set to {reduced}.")
     files = glob.glob(source_dir + "/*.xml")
     docs_indexed = 0
     start = perf_counter()
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
-        futures = [executor.submit(index_file, file, index_name, documents_url, reduced) for file in files]
+        futures = [executor.submit(index_file, file, index_name, reduced) for file in files]
         for future in concurrent.futures.as_completed(futures):
             docs_indexed += future.result()
 
