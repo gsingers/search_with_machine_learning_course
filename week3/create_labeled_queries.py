15acd047518152cae234180bd2d2f1f27d20c9a0
Tim Holland <th0114nd@gmail.com>
2022-07-12 15:54:46 -0400

    Train model to label queries

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -10,15 +10,16 @@ import csv
 import nltk
 stemmer = nltk.stem.PorterStemmer()
 
-categories_file_name = r'./workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
+categories_file_name = r'../workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
-queries_file_name = r'./workspace/datasets/train.csv'
-output_file_name = r'./workspace/datasets/labeled_query_data.txt'
+queries_file_name = r'../workspace/datasets/train.csv'
+output_file_name = r'../workspace/datasets/labeled_query_data.txt'
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 general = parser.add_argument_group("general")
 general.add_argument("--min_queries", default=1,  help="The minimum number of queries per category label (default is 1)")
 general.add_argument("--output", default=output_file_name, help="the file to output to")
+general.add_argument("--stem", default=False, help="whether to stem or not")
 
 args = parser.parse_args()
 output_file_name = args.output
@@ -52,8 +53,9 @@ df = df[df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
 df['query'] = df['query'].str.lower()
-# uncomment when speed is not an issue
-df['query'] = df['query'].apply(stemmer.stem)
+
+if args.stem:
+    df['query'] = df['query'].apply(stemmer.stem)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
 
