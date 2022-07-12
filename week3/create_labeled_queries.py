75a4a09be3f4265be1b4bc50c2368acab79542c7
Tim Holland <th0114nd@gmail.com>
2022-07-12 15:16:59 -0400

    Label queries with category

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from collections import defaultdict
 import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
@@ -9,10 +10,10 @@ import csv
 import nltk
 stemmer = nltk.stem.PorterStemmer()
 
-categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
+categories_file_name = r'./workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
-queries_file_name = r'/workspace/datasets/train.csv'
-output_file_name = r'/workspace/datasets/labeled_query_data.txt'
+queries_file_name = r'./workspace/datasets/train.csv'
+output_file_name = r'./workspace/datasets/labeled_query_data.txt'
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 general = parser.add_argument_group("general")
@@ -48,9 +49,40 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 df = pd.read_csv(queries_file_name)[['category', 'query']]
 df = df[df['category'].isin(categories)]
 
+
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+df['query'] = df['query'].str.lower()
+# uncomment when speed is not an issue
+df['query'] = df['query'].apply(stemmer.stem)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+
+# Roll up categories below the threshold
+# The final keys of category counts are the ones above the threshold
+
+parents_lookup = dict(parents_df.set_index('category')['parent'])
+
+category_counts = defaultdict(int)
+category_counts.update(dict(df.category.value_counts()))
+
+while True:
+    too_small = [cat for cat, count in category_counts.items() if count < min_queries]
+    if not too_small:
+        break
+    for cat in too_small:
+        category_counts[parents_lookup[cat]] += category_counts.pop(cat)
+
+
+# Find the first ancestor that passes the threshold
+category_corrector = {}
+for category in categories:
+    corrected_category = category
+    while corrected_category not in category_counts:
+        corrected_category = parents_lookup[corrected_category]
+    category_corrector[category] = corrected_category
+
+# Correct leafs to their minimum ancestor
+df['category'] = df['category'].apply(lambda c: category_corrector[c])
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
