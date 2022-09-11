5a4cb3d60c20500aaab97775e060540281741fef
dtunkelang <dtunkelang@gmail.com>
2022-09-11 14:09:51 -0700

    removed random sampling to make code deterministic

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -3,7 +3,6 @@ import multiprocessing
 import glob
 from tqdm import tqdm
 import os
-import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
@@ -20,11 +19,6 @@ general.add_argument("--input", default=directory,  help="The directory containi
 general.add_argument("--output", default="/workspace/datasets/fasttext/output.fasttext", help="the file to output to")
 general.add_argument("--label", default="id", help="id is default and needed for downsteam use, but name is helpful for debugging")
 
-# Consuming all of the product data, even excluding music and movies,
-# takes a few minutes. We can speed that up by taking a representative
-# random sample.
-general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
-
 # IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
 general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
 
@@ -37,21 +31,17 @@ if os.path.isdir(output_dir) == False:
 
 if args.input:
     directory = args.input
-# IMPLEMENT:  Track the number of items in each category and only output if above the min
+# IMPLEMENT: Track the number of items in each category and only output if above the min
 min_products = args.min_products
-sample_rate = args.sample_rate
 names_as_labels = False
 if args.label == 'name':
     names_as_labels = True
-
 
 def _label_filename(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     labels = []
     for child in root:
-        if random.random() > sample_rate:
-            continue
         # Check to make sure category name is valid and not in music or movies
         if (child.find('name') is not None and child.find('name').text is not None and
             child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
@@ -70,12 +60,9 @@ def _label_filename(filename):
 
 if __name__ == '__main__':
     files = glob.glob(f'{directory}/*.xml')
-
     print("Writing results to %s" % output_file)
     with multiprocessing.Pool() as p:
         all_labels = tqdm(p.imap_unordered(_label_filename, files), total=len(files))
-
-
         with open(output_file, 'w') as output:
             for label_list in all_labels:
                 for (cat, name) in label_list:
