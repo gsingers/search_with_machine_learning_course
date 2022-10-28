c4a750db444f39ed727f72a7d3c56b57679468fe
Shrikar Archak <shrikar84@gmail.com>
2022-10-28 14:20:05 -0700

    Train

--- week1/utilities/xgb_utils.py
+++ week1/utilities/xgb_utils.py
@@ -11,11 +11,11 @@ def plots(xgb_model, xgb_model_name, xgb_feat_map, xgb_plot):
         bst = xgb.Booster()
         model = bst.load_model(xgb_model)
         model_name = xgb_model_name
-        plt.rcParams["figure.figsize"]=[18,18]
+        plt.rcParams["figure.figsize"] = [18, 18]
         plt.rcParams["figure.autolayout"] = True
         num_trees = len(bst.get_dump(fmap=xgb_feat_map))
-        print("Plotting trees: %s" % (num_trees-1))
-        model_plot = plot_tree(bst, fmap=xgb_feat_map, num_trees=num_trees-1)
+        print("Plotting trees: %s" % (num_trees - 1))
+        model_plot = plot_tree(bst, fmap=xgb_feat_map, num_trees=num_trees - 1)
         model_plot.figure.savefig("%s/%s_tree.png" % (xgb_plot, model_name), dpi=300)
         print("Plotting feature importance")
         impt_plt = plot_importance(bst, fmap=xgb_feat_map)
@@ -25,13 +25,22 @@ def plots(xgb_model, xgb_model_name, xgb_feat_map, xgb_plot):
 
 
 # xgb_train_data is a string path to our training file
-def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
-    xgb_params = {'objective': 'reg:logistic'}
+def train(xgb_train_data, num_rounds=5, xgb_conf=None):
+    xgb_params = {"objective": "reg:logistic"}
     bst = None
     if xgb_conf is not None:
         with open(xgb_conf) as json_file:
             xgb_params = json.load(json_file)
     print("Training XG Boost on %s for %s rounds with params: %s" % (xgb_train_data, num_rounds, xgb_params))
     ##### Step 3.a
+    param = {"max_depth": 5, "silent": 1, "objective": "reg:linear"}
+    print("Training XG Boost")
+    dtrain = xgb.DMatrix(xgb_train_data)
+    bst = xgb.train(
+        param, dtrain, num_rounds
+    )  # Do the training.  NOTE: in this toy example we did not use any hold out data
+    # model = bst.get_dump(fmap=feat_map_file.name, dump_format='json')
+
     print("IMPLEMENT ME: train()")
+    print("TRain done")
     return bst, xgb_params
