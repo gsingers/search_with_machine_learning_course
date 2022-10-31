4dc0545c543b20edc3347c6c1c8da4cffc4a0ec2
Chenyang Zhao <chenyangzhao710@gmail.com>
2022-10-31 05:19:24 +0000

    project

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -236,20 +236,33 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+        #print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
+
+        response = self.opensearch.search(body = log_query, index= self.index_name)
+        features = None
+        if response and response['hits']["hits"] and len(response['hits']['hits']) > 0:
+            hits = response['hits']['hits']
+            features = hits[0]['fields']['_ltrlog'][0]['log_entry']
+
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
+               
+        #rng = np.random.default_rng(12345)
         for doc_id in query_doc_ids:
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
             feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            
+        if features is not None:
+            for feature in features:
+                feature_results[feature.get('name')] = feature.get('value', 0)
+        
+        
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
         # IMPLEMENT_END
--- week1/utilities/xgb_utils.py
+++ week1/utilities/xgb_utils.py
@@ -25,7 +25,7 @@ def plots(xgb_model, xgb_model_name, xgb_feat_map, xgb_plot):
 
 
 # xgb_train_data is a string path to our training file
-def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
+def train(xgb_train_data, num_rounds= 10, xgb_conf=None ):
     xgb_params = {'objective': 'reg:logistic'}
     bst = None
     if xgb_conf is not None:
@@ -33,5 +33,7 @@ def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
             xgb_params = json.load(json_file)
     print("Training XG Boost on %s for %s rounds with params: %s" % (xgb_train_data, num_rounds, xgb_params))
     ##### Step 3.a
-    print("IMPLEMENT ME: train()")
+    #print("IMPLEMENT ME: train()")
+    dtrain = xgb.DMatrix(xgb_train_data)
+    bst = xgb.train(xgb_params, dtrain, num_rounds)
     return bst, xgb_params
