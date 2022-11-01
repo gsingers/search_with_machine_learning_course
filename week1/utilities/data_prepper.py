96b09d4584018e1c419024e6234e3f29e3a1775a
josumsc <josu93alonso@gmail.com>
2022-11-01 19:11:50 +0100

    Reducing logs of implement me

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -236,7 +236,6 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
         # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
         response = self.opensearch.search(body=log_query, index=self.index_name)
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
@@ -245,7 +244,6 @@ class DataPrepper:
         feature_results["query_id"] = []  # ^^^
         feature_results["sku"] = []
         feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
         hits = response['hits']['hits']
         for hit in hits: 
             feature_results["doc_id"].append(hit['_id'])  # capture the doc id so we can join later
--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -74,7 +74,6 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
     query_obj = {
         'size': size,
         'query': {
