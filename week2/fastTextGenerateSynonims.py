2f8d2cd7f030a62ed5a184a43c048615a7aa1252
josumsc <josu93alonso@gmail.com>
2022-11-04 14:03:30 +0100

    Filtering by min_similarity

--- week2/fastTextGenerateSynonims.py
+++ week2/fastTextGenerateSynonims.py
@@ -14,21 +14,26 @@ def get_synonyms(model:fasttext.FastText._FastText, word:str, k:int=5) -> list:
     return synonyms
 
 
-def get_synonyms_from_df(df:pd.DataFrame, model:fasttext.FastText._FastText) -> pd.DataFrame:
+def get_synonyms_from_df(
+    df:pd.DataFrame,
+    model:fasttext.FastText._FastText,
+    min_similarity:float
+) -> pd.DataFrame:
     synonyms_df = pd.DataFrame(columns=['word', 'synonym', 'similarity'])
     for word in df['word']:
         synonyms = get_synonyms(model, word)
         for synonym in synonyms:
             synonyms_df = synonyms_df.append({'word': word, 'synonym': synonym[0], 'similarity': synonym[1]}, ignore_index=True)
-    return synonyms_df
+    return synonyms_df[synonyms_df['similarity'] > min_similarity]
 
 
 input_path = '/workspace/datasets/fasttext/top_words.txt'
 model_path = './title_model_3.bin'
 output_path = '/workspace/datasets/fasttext/synonyms.txt'
+min_similarity = 0.8
 
 if __name__ == '__main__':
     data_df = read_df(input_path)
     model = load_model(model_path)
-    synonyms_df = get_synonyms_from_df(data_df, model)
+    synonyms_df = get_synonyms_from_df(data_df, model, min_similarity)
     synonyms_df.to_csv(output_path, index=False, header=False)
