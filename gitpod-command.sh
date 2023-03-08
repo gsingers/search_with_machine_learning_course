1f3d42fee679da8fe5fd4af44d47b9703a8f260e
Emma M <emma@readyplayeremma.com>
2023-03-08 06:12:49 +0000

    Explained the gitpod chown thing with a commant

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -2,8 +2,11 @@ ln -s /workspace/kaggle/ /home/gitpod/.kaggle
 
 mkdir -p /workspace/opensearch
 mkdir -p /workspace/datasets
-sudo chown -R 1000:1000 /workspace/opensearch
 
+# If we want OpenSearch to be able to put the data here we have to give
+# filesystem permissions to the UID/GID that OpenSearch is running as
+# inside its container.
+sudo chown -R 1000:1000 /workspace/opensearch
 
 mkdir /workspace/ltr_output
 cp data/validity.csv /workspace/ltr_output/
