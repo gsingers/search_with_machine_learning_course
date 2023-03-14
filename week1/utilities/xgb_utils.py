21daa96ed8f89e00681092e6cac9da8ad9bc39c6
Parima Shah <parima@outschool.com>
2023-03-14 04:16:31 +0000

    Week 1 Assignment

--- week1/utilities/xgb_utils.py
+++ week1/utilities/xgb_utils.py
@@ -34,4 +34,7 @@ def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
     print("Training XG Boost on %s for %s rounds with params: %s" % (xgb_train_data, num_rounds, xgb_params))
     ##### Step 3.a
     print("IMPLEMENT ME: train()")
+    dtrain = xgb.DMatrix(xgb_train_data)
+    bst = xgb.train(xgb_params, dtrain,
+                num_round)
     return bst, xgb_params
