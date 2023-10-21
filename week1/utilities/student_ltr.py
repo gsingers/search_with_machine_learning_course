94fd975215b01a83bef4762c2156505f1c4ee80f
GuilhermGuitte <guilherme.guitte@gmail.com>
2023-10-21 13:24:45 +0200

    feat(lvl-2): try different features

--- week1/utilities/student_ltr.py
+++ week1/utilities/student_ltr.py
@@ -100,12 +100,15 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
             "rescore-query": {
                 "sltr": {
                     "params": {
-                        "keywords": user_query
+                        "keywords": user_query,
+                        "skus": user_query.split(),
+                        "click_prior_query": click_prior_query
                     },
                     "model": ltr_model_name,
                     "store": ltr_store_name
                 }
             },
+            "score_mode": "total",
             "query_weight": main_query_weight,
             "rescore_query_weight": rescore_query_weight
         }
@@ -125,16 +128,14 @@ and extract the features into a data frame.
 
 
 def extract_logged_features(hits, query_id):
-    import numpy as np
     import pandas as pd
-    print(
-        "IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+
     feature_results = {}
     feature_results["doc_id"] = []  # capture the doc id so we can join later
     feature_results["query_id"] = []  # ^^^
     feature_results["sku"] = []
     feature_results["name_match"] = []
-    rng = np.random.default_rng(12345)
+
     for (idx, hit) in enumerate(hits):
         feature_results["doc_id"].append(int(hit['_id']))  # capture the doc id so we can join later
         feature_results["query_id"].append(query_id)  # super redundant, but it will make it easier to join later
